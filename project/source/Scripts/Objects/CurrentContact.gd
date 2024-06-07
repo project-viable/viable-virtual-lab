f3a9f7ebc1836d25da4d3e59c9d10c2c6a2f0c7a
tylerhand1 <tyha7462@colorado.edu>
2024-06-07 11:46:28 -0600

    Fixed calling dispose on CurrentContact that is a part of a ContactWire so that it calls dispose on ContactWire instead

--- project/source/Scripts/Objects/CurrentContact.gd
+++ project/source/Scripts/Objects/CurrentContact.gd
@@ -1,10 +1,19 @@
 extends LabObject
 
 export (bool) var positive = true
+var regex = RegEx.new()
 
 func _ready():
 	if(!positive):
 		$Contact.texture = load('res://Images/NegativeContact.png')
+	regex.compile("ContactWire")
 
 func is_positive():
 	return positive
+
+func dispose():
+	var parentIsContactWire = regex.search(get_parent().name)
+	if parentIsContactWire:
+		get_parent().dispose()
+	else:
+		self.queue_free()
