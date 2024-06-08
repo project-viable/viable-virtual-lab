dc444714338c893d4ba2cf2e58fc4530aed320fc
jcourt325 <jcourt325@gmail.com>
2024-06-08 16:35:28 -0600

    Added vertical camera movement, and Input Axes

--- project/source/Scripts/UI/Camera.gd
+++ project/source/Scripts/UI/Camera.gd
@@ -12,11 +12,15 @@ func _process(delta):
 		translate(Vector2(-moveRate, 0))
 	if Input.is_action_pressed("CameraRight"):
 		translate(Vector2(moveRate, 0))
+	if Input.is_action_pressed("CameraUp"):
+		translate(Vector2(0, -moveRate))
+	if Input.is_action_pressed("CameraDown"):
+		translate(Vector2(0, moveRate))
 	
 	if dragging:
 		#adjust our position so the mouse is still at the same global coordinate in the scene
 		var currentOffset = get_global_mouse_position() - dragMousePosition
-		global_position -= Vector2(currentOffset.x, 0)
+		global_position -= currentOffset
 		
 		if Input.is_action_just_released("DragCamera"):
 			StopDragging()
