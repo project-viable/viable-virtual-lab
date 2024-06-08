f04445ce649fd6bb62c10f9a97c087b58e81268e
jcourt325 <jcourt325@gmail.com>
2024-06-08 16:48:07 -0600

    Added camera zoom

--- project/source/Scripts/UI/Camera.gd
+++ project/source/Scripts/UI/Camera.gd
@@ -1,6 +1,8 @@
 extends Camera2D
 
-var moveRate = 12.5
+var baseMoveRate = 12.5 #the rate at which to move when zoom is 1.
+onready var moveRate = baseMoveRate #we adjust the move rate to be visually constant when we zoom in or out
+var zoomFactor = 1.1 #we multiple/divide the zoom.
 
 #these variables are used internally to handle clicking and dragging:
 var dragging = false
@@ -17,6 +19,8 @@ func _process(delta):
 	if Input.is_action_pressed("CameraDown"):
 		translate(Vector2(0, moveRate))
 	
+	#The camera cannot check for mouse input directly because of how input is handled. See docs for the Main Scene.
+	
 	if dragging:
 		#adjust our position so the mouse is still at the same global coordinate in the scene
 		var currentOffset = get_global_mouse_position() - dragMousePosition
@@ -27,6 +31,8 @@ func _process(delta):
 
 func Reset():
 	position = Vector2(0, 0)
+	zoom = Vector2(1, 1)
+	moveRate = baseMoveRate
 
 #Camera does not do this on it's own. When input happens, Main decides what should happen.
 func StartDragging():
@@ -35,3 +41,13 @@ func StartDragging():
 
 func StopDragging():
 	dragging = false
+
+#This function is called by the Main Scene. Read the documentation for that for why.
+func ZoomIn():
+	zoom = zoom / zoomFactor
+	moveRate = moveRate / zoomFactor
+
+#This function is called by the Main Scene. Read the documentation for that for why.
+func ZoomOut():
+	zoom = zoom * zoomFactor
+	moveRate = moveRate * zoomFactor
