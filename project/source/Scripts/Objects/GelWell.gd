93518fa5e28e1e0bd0642dcda6833149f1c0a136
jcourt325 <jcourt325@gmail.com>
2024-07-14 22:49:22 -0600

    incomplete changes

---
+++ project/source/Scripts/Objects/GelWell.gd
@@ -0,0 +1,41 @@
+extends LabObject
+
+var broken = false
+
+func AddContents(new_contents):
+	#TODO: This could do the wrong thing if an object other than the pipette being handled by the Proxy tries to add liquid.
+	print("-gel well add contents")
+	
+	#If the well is broken, you can't add stuff to it.
+	if broken:
+		AddLow(new_contents)
+	
+	###See what the thing is colliding with:
+	#check low
+	for other in $LowArea.get_overlapping_bodies():
+		if other is Pipette:
+			AddLow(new_contents)
+			return
+	#check mid
+	for other in $MidArea.get_overlapping_bodies():
+		if other is Pipette:
+			AddMid(new_contents)
+			return
+	#check high
+	for other in $HighArea.get_overlapping_bodies():
+		if other is Pipette:
+			AddHigh(new_contents)
+			return
+
+func AddHigh(new_contents):
+	LabLog.Warn("Added too High")
+	#TODO: the contents go nowhere. Is this correct?
+
+func AddMid(new_contents):
+	LabLog.Log("Added a substance to the gel well")
+	GetSubsceneManagerParent().AddContents(new_contents)
+
+func AddLow(new_contents):
+	broken = true
+	LabLog.Warn("Added too Low")
+	#TODO: the contents go nowhere. Is this correct?
--- project/source/Scripts/Objects/GelWellPipetteProxy.gd
+++ project/source/Scripts/Objects/GelWellPipetteProxy.gd
@@ -1,12 +1,18 @@
 extends LabObject
 
 var adoptedObject = null
+onready var startPos = global_position
 
 func TryInteract(others):
 	for other in others:
-		if other is Pipette:
+		if other is Pipette and adoptedObject == null:
 			AdoptPipette(other)
 			return true
+		elif other.is_in_group("GelWell"):
+			if adoptedObject:
+				adoptedObject.TryInteract([other])
+				ReleasePipette()
+				return true
 	return false
 
 func AdoptPipette(pipette):
@@ -17,6 +23,15 @@ func AdoptPipette(pipette):
 	#set ourselves up
 	draggable = true
 	adoptedObject = pipette
+
+func ReleasePipette():
+	#set the pipette up
+	adoptedObject.draggable = true
+	
+	#set ourselves up
+	draggable = false
+	adoptedObject = null
+	global_position = startPos
 
 func DragMove():
 	global_position = Vector2(global_position.x, (get_global_mouse_position() - dragOffset).y)
