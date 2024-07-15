55d33f85f8b79f88620260a52b28ea7867fd44a0
jcourt325 <jcourt325@gmail.com>
2024-07-15 04:38:10 -0600

    Proxies stop animating when in use, there's now only 5 wells

--- project/source/Scripts/Objects/GelWellPipetteProxy.gd
+++ project/source/Scripts/Objects/GelWellPipetteProxy.gd
@@ -23,13 +23,14 @@ func AdoptPipette(pipette):
 	#set ourselves up
 	draggable = true
 	adoptedObject = pipette
+	$AnimationPlayer.stop(true)
 
 func ReleasePipette():
 	#set the pipette up
 	adoptedObject.draggable = true
 	
 	#set ourselves up
-	queue_free()
+	queue_free() #TODO: ideally you'd be able to try again, if you didn't break the well.
 
 func DragMove():
 	global_position = Vector2(global_position.x, (get_global_mouse_position() - dragOffset).y)
