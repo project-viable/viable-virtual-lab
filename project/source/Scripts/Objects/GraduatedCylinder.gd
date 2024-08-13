0f9b094dd76be7fc727fdc7f27c57a721efdc541
tylerhand1 <tyha7462@colorado.edu>
2024-08-13 17:37:25 -0600

    Fixed issue of graduated cylinder filling with nothing, which caused issues when accidentally trying to overfill it

--- project/source/Scripts/Objects/GraduatedCylinder.gd
+++ project/source/Scripts/Objects/GraduatedCylinder.gd
@@ -4,7 +4,9 @@ export (int) var maxVolume
 
 var allowedGroups = ["Source Container"]
 
-var defaultText: String
+var DefaultText: String
+
+var CurrContent
 
 func LabObjectReady():
 	if maxVolume == 0:
@@ -23,31 +25,28 @@ func TryInteract(others):
 	for other in others:
 		for i in allowedGroups.size():
 			if other.is_in_group(allowedGroups[i]):
+				# Continue through loop if the graduated cylinder is already full
+				if $VolumeContainer.GetVolume() == $VolumeContainer.GetMaxVolume():
+					continue
 				# Add contents to grad cylinder if it has nothing and the other container has a liquid substance
 				# Or if adding more of same liquid substance
 				# Set volume of grad cylinder to its max until a menu is created to specify volume
-				print("Other:", other)
 				if len(contents) == 0 and other.CheckContents("Liquid Substance") \
 					or len(contents) > 0 and other.contents.name == contents[0].name:
 					$Menu.visible = true
-					var oldVolume = $VolumeContainer.GetVolume()
+					
+					# We want to store the substance in a variable
+					# so that it can be accessed in _on_DispenseButton_pressed()
+					CurrContent = other
 					
 					# Disable dragging while menu open
 					self.draggable = false
 					yield($Menu/PanelContainer/VBoxContainer/DispenseButton, "pressed")
 					# Reenable dragging
 					self.draggable = true
-					# Check if the grad cylinder's substance volume has changed
-					if oldVolume != $VolumeContainer.GetVolume() and len(contents) < 1:
-						contents.append_array(other.TakeContents($VolumeContainer.GetVolume()))
-						
-						# Update the volume of the contents
-						contents[0].set_volume($VolumeContainer.GetVolume())
-						LabLog.Log("Added " + str(contents[0].get_volume()) + "mL of " + contents[0].name + " to graduated cylinder.")
-					update_display()
 				# Other is a container with a liquid substance and grad cylinder already has liquid, so do nothing
 				else:
-					return false
+					continue
 				update_display()
 				print("Graduated cylinder has ", $VolumeContainer.GetVolume(), "mL of liquid")
 				return true
@@ -116,9 +115,9 @@ func update_display():
 
 # Reset values in menu
 func ResetMenu():
-	defaultText = "Graduated Cylinder currently has a " \
+	DefaultText = "Graduated Cylinder currently has a " \
 		+ "volume of " + String($VolumeContainer.GetVolume()) + "mL"
-	$Menu/PanelContainer/VBoxContainer/Description.text = defaultText
+	$Menu/PanelContainer/VBoxContainer/Description.text = DefaultText
 	$Menu/PanelContainer/VBoxContainer/SpinBox.set_value(0)
 	update_display()
 
@@ -129,9 +128,14 @@ func _on_CloseButton_pressed():
 func _on_DispenseButton_pressed():
 	var substanceVolume = $Menu/PanelContainer/VBoxContainer/SpinBox.value
 	if not $VolumeContainer.AddSubstance(substanceVolume):
-		$Menu/PanelContainer/VBoxContainer/Description.text = defaultText + "\nWarning: Cannot add " \
+		$Menu/PanelContainer/VBoxContainer/Description.text = DefaultText + "\nWarning: Cannot add " \
 			+ String(substanceVolume) + "mL to container"
 	else:
+		contents.append_array(CurrContent.TakeContents($VolumeContainer.GetVolume()))
+		# Update the volume of the contents
+		contents[0].set_volume(substanceVolume)
+		LabLog.Log("Added " + str(contents[0].get_volume()) + "mL of " + contents[0].name + " to graduated cylinder.")
+		update_display()
 		ResetMenu()
 
 func draw():
