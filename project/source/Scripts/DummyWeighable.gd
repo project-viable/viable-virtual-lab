98a26d85959ee46b2f1ae72938c0a8ab0ccff9f0
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-10 16:19:33 -0700

    Add types to DummyWeighable.gd

--- project/source/Scripts/DummyWeighable.gd
+++ project/source/Scripts/DummyWeighable.gd
@@ -1,15 +1,17 @@
 extends LabObject
 
-var contents = null
-var substance = null
+var contents: Substance = null
+var substance: PackedScene = null
 
-func CheckContents(group):
+func CheckContents(group: String) -> bool:
 	return contents.is_in_group(group)
 
-func TakeContents():
+func TakeContents() -> Array[Substance]:
+	# TODO (update): We should probably return [] here instead of null. The array type doesn't
+	# allow null anyway.
 	if substance == null:
 		return null
 	
-	var new_content = substance.instantiate()
+	var new_content: Substance = substance.instantiate()
 	print("Dispensed some of the stored substance")
 	return [new_content]
