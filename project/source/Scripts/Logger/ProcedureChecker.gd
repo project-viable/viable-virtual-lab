6a4b012ad7403c1cd0a7c5073a2a951a94eb0a55
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-10 16:31:43 -0700

    Add types to ProcedureChecker.gd

--- project/source/Scripts/Logger/ProcedureChecker.gd
+++ project/source/Scripts/Logger/ProcedureChecker.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 @export var CheckStrategies: Array[MistakeChecker]
 
-func CheckAction(params: Dictionary):
+func CheckAction(params: Dictionary) -> void:
 	for strategy in CheckStrategies:
 		strategy.CheckAction(params)
