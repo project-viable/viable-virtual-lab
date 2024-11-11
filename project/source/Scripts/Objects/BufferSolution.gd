480c025f0d34db1637cc2cbc4f529b148a998648
Mariana Ochoa <maoc1414@colorado.edu>
2024-11-11 15:46:25 -0700

    added static typing to buffer solution, agar powder, and binder substance

--- project/source/Scripts/Objects/BufferSolution.gd
+++ project/source/Scripts/Objects/BufferSolution.gd
@@ -3,24 +3,24 @@ extends LabObject
 # Currently the same as DummySourceContainer to use for testing
 
 @export var substance: PackedScene = null
-var contents = null
+var contents:Node = null
 
-func _ready():
+func _ready() -> void:
 	super()
 	if substance == null:
 		substance = load('res://Scenes/Objects/BufferSolutionSubstance.tscn')
 	contents = substance.instantiate()
 
-func CheckContents(group):
+func CheckContents(group:String) -> bool:
 	return contents.is_in_group(group)
 
-func TakeContents():
+func TakeContents() -> Array:
 	if substance == null:
-		return null
+		return [null]
 	
-	var new_content = substance.instantiate()
+	var new_content:Node = substance.instantiate()
 	print("Dispensed some of TAE Buffer Solution")
 	return [new_content]
 
-func AddContents(new_contents):
+func AddContents(new_contents:Array) -> void:
 	pass
