a99c8cde3a4f9386817c8b42bc3953432a441ce9
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-11 13:26:26 -0700

    Add a TODO

--- project/source/Scripts/DummyWeighable.gd
+++ project/source/Scripts/DummyWeighable.gd
@@ -3,6 +3,8 @@ extends LabObject
 var contents: Substance = null
 var substance: PackedScene = null
 
+# TODO (update): The other `CheckContents` functions seem to return an `Array[bool]` with a `bool`
+# for each substance contained. This should do the same.
 func CheckContents(group: String) -> bool:
 	return contents.is_in_group(group)
 
