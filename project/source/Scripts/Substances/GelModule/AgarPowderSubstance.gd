5c41894b60c99f06bd8797a35d80365b9e79557d
Mariana Ochoa <maoc1414@colorado.edu>
2024-11-11 15:46:25 -0700

    added static typing to buffer solution, agar powder, and binder substance

--- project/source/Scripts/Substances/GelModule/AgarPowderSubstance.gd
+++ project/source/Scripts/Substances/GelModule/AgarPowderSubstance.gd
@@ -2,8 +2,8 @@ extends "res://Scripts/Substances/Substance.gd"
 
 # This represents the powdered agarose that makes up one part of the gel.
 
-func _init():
-	var properties = {
+func _init() -> void:
+	var properties:Dictionary = {
 		'density': 1,
 		'color': '#ffffff'
 	}
