b61d071a476024cd3a7cdfcffc0279c7e805efc1
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-11 15:30:17 -0700

    Add types to DNASubstance.gd

--- project/source/Scripts/Substances/GelModule/DNASubstance.gd
+++ project/source/Scripts/Substances/GelModule/DNASubstance.gd
@@ -2,22 +2,28 @@ extends "res://Scripts/Substances/Substance.gd"
 class_name DNASubstance
 
 #export (Array) var particle_sizes = [1,1.3,1.7,3]
-var particle_sizes = [1,1.3,1.7,3]
+var particle_sizes: Array[float] = [1,1.3,1.7,3]
 
-func _init():
-	var properties = {
+func _init() -> void:
+	var properties := {
 		'density': 1.2,
 		'color': '#00cc66'
 	}
 	init_created(properties)
 
-func initialize(input_particle_sizes):
+# TODO (update): This should really just be a constructor. If we made `Substance` derive from
+# `Resource` instead, it might make stuff like this easier. No nodes should have to be instantiated
+# to make a substance.
+func initialize(input_particle_sizes: Array[float]) -> void:
 	particle_sizes = input_particle_sizes
 
-func get_particle_sizes():
+# TODO (update): This is never used. Every other function that accesses this class instead opts to
+# directly read from `particle_sizes`, so we should figure out how we want to handle it
+# consistently.
+func get_particle_sizes() -> Array[float]:
 	return particle_sizes
 
-func get_properties():
+func get_properties() -> Dictionary:
 	return {
 		"color": color,
 		"volume": volume
