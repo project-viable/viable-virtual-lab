44792d92d4b5d5309e060080a471759defa309af
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-11 16:27:19 -0700

    Add types to MixManager.gd

--- project/source/Scripts/UI/MixManager.gd
+++ project/source/Scripts/UI/MixManager.gd
@@ -10,30 +10,31 @@ extends Node
 # the default value when extending this class for modules. [!]
 
 # here, mixing two default substances yields another default substance
-var outcomes = {
+var outcomes: Dictionary = {
 	['Substance', 'Substance'] : 'Substance'
 }
-var substance_folder = 'res://Scenes/Substances/'
+var substance_folder: String = 'res://Scenes/Substances/'
 
-func _ready():
+func _ready() -> void:
 	# ensure all outcome key arrays are sorted
-	var keys = outcomes.keys()
-	for key in keys:
+	var keys: Array[Array] = outcomes.keys()
+	for key: Array[Substance] in keys:
 		key.sort()
 
-func mix(reactants):
+# TODO (update): Make sure all paths return a value.
+func mix(reactants: Array[Substance]) -> Substance:
 	# check if this outcome is listed in the outcomes dictionary
-	var check_array = []
+	var check_array: Array[String] = []
 	for reactant in reactants:
 		check_array.append(reactant.name)
 	check_array.sort() # all key arrays must be sorted, or matching will fail
 	
 	if(outcomes.has(check_array)):
 		# load and create the new substance
-		var result_name = outcomes[check_array]
-		var result_ref = load(substance_folder + result_name + '.tscn')
+		var result_name: String = outcomes[check_array]
+		var result_ref: PackedScene = load(substance_folder + result_name + '.tscn')
 		if(result_ref != null):
-			var result = result_ref.instantiate()
+			var result := result_ref.instantiate()
 			# Need to add to scene in order to be able to call MistakeChecker methods
 			self.add_child(result)
 			result.init_mixed(reactants)
