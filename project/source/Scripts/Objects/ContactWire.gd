0a96cff8f808573a9ee772051a6a60cef0c8b83b
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-10 17:04:41 -0700

    Add types to ContactWire.gd

--- project/source/Scripts/Objects/ContactWire.gd
+++ project/source/Scripts/Objects/ContactWire.gd
@@ -3,15 +3,16 @@ class_name ContactWire
 enum {REVERSE, FORWARD, NEUTRAL}
 
 @export var positive: bool = true
-var connections = []
+var connections: Array[Node] = []
 
-var point1 : Vector2
-var point2 : Vector2
-var width = 2
-var color = Color.BLACK
-var current_direction = NEUTRAL
+var point1: Vector2
+var point2: Vector2
 
-func _ready():
+var width := 2.0
+var color := Color.BLACK
+var current_direction := NEUTRAL
+
+func _ready() -> void:
 	if(!positive):
 		$Contact1.positive = false
 		$Contact2.positive = false
@@ -22,21 +23,22 @@ func _ready():
 		point1 = $Contact1.position
 		point2 = $Contact2.position
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if $Contact1 and $Contact2:
-		var contact1_position = $Contact1.position
-		var contact2_position = $Contact2.position
+		var contact1_position: Vector2 = $Contact1.position
+		var contact2_position: Vector2 = $Contact2.position
 		
 		if point1 != contact1_position or point2 != contact2_position:
 			point1 = contact1_position
 			point2 = contact2_position
+
 			update()
 
-func _draw():
+func _draw() -> void:
 	draw_line(point1, point2, color, width)
 
-func is_positive():
+func is_positive() -> bool:
 	return positive
 
-func dispose():
+func dispose() -> void:
 	self.queue_free()
