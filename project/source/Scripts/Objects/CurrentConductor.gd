37195402b98cc4a704064988b24808257b0377f0
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-11-10 17:07:19 -0700

    Add types to CurrentConductor.gd

--- project/source/Scripts/Objects/CurrentConductor.gd
+++ project/source/Scripts/Objects/CurrentConductor.gd
@@ -2,22 +2,22 @@
 extends Node2D
 class_name CurrentConductor
 
-var volts = 0
-var time = 0 # in minutes
+var volts := 0.0
+var time := 0.0 # in minutes
 
-func _ready():
+func _ready() -> void:
 	pass
 	
-func GetVolts():
+func GetVolts() -> float:
 	return volts
 
-func SetVolts(newVolts):
+func SetVolts(newVolts: float) -> void:
 	if newVolts >= 0:
 		volts = newVolts
 
-func GetTime():
+func GetTime() -> float:
 	return time
 	
-func SetTime(newTime):
+func SetTime(newTime: float) -> void:
 	if time >= 0:
 		time = newTime
