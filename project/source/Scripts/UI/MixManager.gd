b44cc0b128028057be26e0fea9e85dc08b69eb1a
Harper Stoeckle <harper.stoeckle@gmail.com>
2024-12-04 19:53:44 -0700

    Ensure all code paths return in MixManager::mix

--- project/source/Scripts/UI/MixManager.gd
+++ project/source/Scripts/UI/MixManager.gd
@@ -21,7 +21,6 @@ func _ready() -> void:
 	for key: Array[Substance] in keys:
 		key.sort()
 
-# TODO (update): Make sure all paths return a value.
 func mix(reactants: Array[Substance]) -> Substance:
 	# check if this outcome is listed in the outcomes dictionary
 	var check_array: Array[String] = []
@@ -39,5 +38,5 @@ func mix(reactants: Array[Substance]) -> Substance:
 			self.add_child(result)
 			result.init_mixed(reactants)
 			return result
-	else:
-		return null
+	
+	return null
