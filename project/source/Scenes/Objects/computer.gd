fb8b6dc4d035d7824625ac27242720b3654c5233
Derick Sayavong <dsayavong54651@gmail.com>
2025-01-28 18:38:43 -0700

    Zoom functionality when clicking on the computer

---
+++ project/source/Scenes/Objects/computer.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+signal screen_click_signal()
+var is_clicked: bool = false
+
+# Emits a signal to the FlourescenceMicroscope Node, used to zoom into the computer screen
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and not is_clicked:
+		print("he")
+		screen_click_signal.emit()
+		is_clicked = true
