350ff4a58b1b85f054b189bc36d4e056e2888088
EveDiby <94596452+EveDiby@users.noreply.github.com>
2025-01-31 15:15:07 -0700

    Updated variable names to match style guide
    Changed the variable names from CamelCase to SnakeCase.

--- project/source/Scripts/Objects/JoystickKnob.gd
+++ project/source/Scripts/Objects/JoystickKnob.gd
@@ -1,47 +1,47 @@
 extends Sprite2D
 
-@export var maxDragLength: float = 100 #Max length the joystick can be dragged out to 
-@export var deadZone: float = 5
+@export var max_drag_length: float = 100 #Max length the joystick can be dragged out to 
+@export var deadzone: float = 5
 
-@onready var joystickParent: Node2D  = $".."
+@onready var joystick_parent: Node2D  = $".."
 
-var buttonPressed : bool = false
+var button_pressed : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#Even if we change the size of the current joystick, the scale will be proportional
-	maxDragLength * joystickParent.scale.x
+	max_drag_length * joystick_parent.scale.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if buttonPressed:
-		if get_global_mouse_position().distance_to(joystickParent.global_position) <= maxDragLength:
+	if button_pressed:
+		if get_global_mouse_position().distance_to(joystick_parent.global_position) <= max_drag_length:
 			global_position = get_global_mouse_position()
 		else:
 			#Even if the mouse is outside of the bounds of the max drag length, you can still 
 			#control the joystick will still pressing down
-			var angle: float = joystickParent.global_position.angle_to_point(get_global_mouse_position())
-			global_position.x = joystickParent.global_position.x + cos(angle)*maxDragLength
-			global_position.y = joystickParent.global_position.y + sin(angle)*maxDragLength
+			var angle: float = joystick_parent.global_position.angle_to_point(get_global_mouse_position())
+			global_position.x = joystick_parent.global_position.x + cos(angle)*max_drag_length
+			global_position.y = joystick_parent.global_position.y + sin(angle)*max_drag_length
 		##For debugging purposes so that you can know the vectoro of the joystick
 		#calculateVector()
 	else:
 		#Joystick position resets when the user lets go
-		global_position = lerp(global_position, joystickParent.global_position, delta*10)
+		global_position = lerp(global_position, joystick_parent.global_position, delta*10)
 		##For debugging purposes so that you can know the vectoro of the joystick
-		#joystickParent.posVector = Vector2(0,0)
-		#print(joystickParent.posVector)
+		#joystick_parent.posVector = Vector2(0,0)
+		#print(joystick_parent.posVector)
 
 ##For debugging purposes so that you can know the vectoro of the joystick
 #func calculateVector() -> void:
-	#if abs(global_position.x - joystickParent.global_position.x) >= deadZone:
-		#joystickParent.posVector.x = (global_position.x - joystickParent.global_position.x)/ maxDragLength
-	#if abs(global_position.y - joystickParent.global_position.y) >= deadZone:
-		#joystickParent.posVector.y = (global_position.y - joystickParent.global_position.y)/ maxDragLength
+	#if abs(global_position.x - joystick_parent.global_position.x) >= deadzone:
+		#joystick_parent.posVector.x = (global_position.x - joystick_parent.global_position.x)/ max_drag_length
+	#if abs(global_position.y - joystick_parent.global_position.y) >= deadzone:
+		#joystick_parent.posVector.y = (global_position.y - joystick_parent.global_position.y)/ max_drag_length
 
 func _on_button_button_down() -> void:
-	buttonPressed = true
+	button_pressed = true
 
 
 func _on_button_button_up() -> void:
-	buttonPressed =  false
+	button_pressed =  false
