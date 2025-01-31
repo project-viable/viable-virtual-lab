1dfc38e49da9e66e7a7da85fa83cc533d0c0f47b
EveDiby <94596452+EveDiby@users.noreply.github.com>
2025-01-31 15:11:50 -0700

    Initial virtual joystick added.
    Virtual Joystick with temporary sprites. Will need to tie it's movement to the camera of the microscope.

--- project/source/Scripts/Objects/Joystick.gd
+++ project/source/Scripts/Objects/Joystick.gd
@@ -1,24 +1,29 @@
 extends RigidBody2D
+#Could try doing this with a virtual joystick setup and having users click and then drag 
+var posVector : Vector2
+
+
+
+
 # we could change this to extend LabObject but I don't know if
 # we want to use that or not
 
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		# just using 1 for now since i don't know how much it'll actually move yet
-		# also not sure if we want pressing right = move right or if
-		# we want press right = move left
-		position.x += 1
-	elif Input.is_action_just_pressed("ui_left"):
-		position.x -= 1
-	elif Input.is_action_just_pressed("ui_up"):
-		# same with y-axis, press up = move up or press up = move down?
-		position.y += 1
-	elif Input.is_action_just_pressed("ui_down"):
-		position.y -= 1
-		
+#func _ready() -> void:
+	#pass
+	#
+#func _process(delta: float) -> void:
+	#if Input.is_action_pressed("ui_right"):
+		## just using 1 for now since i don't know how much it'll actually move yet
+		## also not sure if we want pressing right = move right or if
+		## we want press right = move left
+		#position.x += 1
+	#elif Input.is_action_just_pressed("ui_left"):
+		#position.x -= 1
+	#elif Input.is_action_just_pressed("ui_up"):
+		## same with y-axis, press up = move up or press up = move down?
+		#position.y += 1
+	#elif Input.is_action_just_pressed("ui_down"):
+		#position.y -= 1
 		
 # this is another way of doing the same thing but extending a 
 # CharacterBody2D with an input map 
