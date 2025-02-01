e2591836ee7b897d2acfd8adb103a67233e6a991
Derick Sayavong <dsayavong54651@gmail.com>
2025-01-31 20:27:52 -0700

    Fixed image blue when moving camera

--- project/source/Scenes/Objects/main_screen.gd
+++ project/source/Scenes/Objects/main_screen.gd
@@ -2,26 +2,29 @@ extends Node2D
 
 var cell_image_width: float 
 var cell_image_height: float
-
+var speed: float = 500
 func _ready() -> void:
 	cell_image_width = $CellImage/Sprite2D.texture.get_width()
 	cell_image_height = $CellImage/Sprite2D.texture.get_height()
+
 	
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	# Should only be able to move the camera if the screen is actually visible
 	if visible:
 		var direction:Vector2 = Input.get_vector("CameraLeft", "CameraRight", "CameraUp", "CameraDown")
-		$CellImage/Sprite2D.region_rect.position += direction
+		$CellImage/Sprite2D.region_rect.position += direction * speed * delta
+		
+		# Check so the visible image doesnt go past the actual image width and height
 		if $CellImage/Sprite2D.region_rect.position.x <= 0:
 			$CellImage/Sprite2D.region_rect.position.x = 0
-		if $CellImage/Sprite2D.region_rect.position.x >= cell_image_width:
-			$CellImage/Sprite2D.region_rect.position.x = cell_image_width
 			
-		if $CellImage/Sprite2D.region_rect.position.y >= cell_image_height:
-			$CellImage/Sprite2D.region_rect.position.y = cell_image_height
+		if $CellImage/Sprite2D.region_rect.position.x >= cell_image_width - $CellImage/Sprite2D.region_rect.size.x:
+			$CellImage/Sprite2D.region_rect.position.x = cell_image_width - $CellImage/Sprite2D.region_rect.size.x
+			
+		if $CellImage/Sprite2D.region_rect.position.y >= cell_image_height - $CellImage/Sprite2D.region_rect.size.y:
+			$CellImage/Sprite2D.region_rect.position.y = cell_image_height - $CellImage/Sprite2D.region_rect.size.y
 			
 		if $CellImage/Sprite2D.region_rect.position.y <= 0:
-			
 			$CellImage/Sprite2D.region_rect.position.y = 0 
 		
 	
