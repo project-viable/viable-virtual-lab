45f8445369539a0d85ca8f205a8840baed05df0a
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-03-15 23:58:34 -0600

    Replace the log dictionary with a LogMessage class

--- project/source/Scripts/LabLogSingleton.gd
+++ project/source/Scripts/LabLogSingleton.gd
@@ -1,37 +1,25 @@
 extends Node
 #This node should be autoloaded, so that it's always accessible from everywhere
 
-#logs should generally be in one of 3 categories: "log", "warning", or "error".
-#log is for general messages - not mistakes, just something you want to log.
-#warning is for minor errors that you can continue on from - like putting glass in a nornmal trash can
-#error is for things you can't recover from
-#^but, this system is designed to support other weird things, if you want.
-#logs is a dictionary like this:
-#{'category1': [list of logs], 'category2': [list of logs]}
-#each log is stored as dictionaries like this:
-#{'message': "something", 'hidden': false, 'popup': false}
 #log data will be used by other things. This node just stores them and notifies others when they change.
-var logs: Dictionary = {}
-signal new_message(category: String, new_log: Dictionary)
+var logs: Array[LogMessage] = []
+
+signal new_message(new_log: LogMessage)
 signal logs_cleared()
 signal ReportShown()
 
-func add_log_message(category: String, message: String, hidden: bool = false, popup: bool = false) -> void:
-	if not logs.has(category):
-		logs[category] = []
-	
-	var new_log: Dictionary = {'message': message, 'hidden': hidden, 'popup': popup}
-	logs[category].append(new_log)
-	
-	emit_signal("new_message", category, new_log)
+func add_log_message(category: LogMessage.Category, message: String, hidden: bool = false, popup: bool = false) -> void:
+	var new_log := LogMessage.new(category, message, hidden, popup)
+	logs.append(new_log)
+	emit_signal("new_message", new_log)
 
-#returns the entire logs structure, defined above.
-func get_logs() -> Dictionary:
-	return logs.duplicate(true)
+# Get logs with the category `category`.
+func get_logs(category: LogMessage.Category) -> Array[LogMessage]:
+	return logs.filter(func (l: LogMessage) -> bool: return l.category == category)
 
 #clears all the logs
 func clear_logs() -> void:
-	logs = {}
+	logs.clear()
 	emit_signal("logs_cleared")
 
 #This is just for convenience - instead of having to find the final report UI node, just call LabLog.show_report() and it'll make sure that everyone who needs to know about it does.
@@ -43,11 +31,11 @@ func show_report() -> void:
 #the intent is that you should only ever have to call these
 
 func log(message: String, hidden: bool = false, popup: bool = true) -> void:
-	add_log_message("log", message, hidden, popup)
+	add_log_message(LogMessage.Category.LOG, message, hidden, popup)
 
 func warn(message: String, hidden: bool = false, popup: bool = true) -> void:
-	add_log_message("warning", message, hidden, popup)
+	add_log_message(LogMessage.Category.WARNING, message, hidden, popup)
 
 #errors should generally always popup, and should not be hidden.
 func error(message: String) -> void:
-	add_log_message("error", message, false, true)
+	add_log_message(LogMessage.Category.ERROR, message, false, true)
