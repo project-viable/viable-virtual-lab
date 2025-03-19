a50baa201ee5ffbcaa12d32c165a9e296777b816
Derick Sayavong <dsayavong54651@gmail.com>
2025-03-18 23:46:36 -0600

    Changed image file names such that we can use string interporlation

--- project/source/Scenes/Objects/content_screen.gd
+++ project/source/Scenes/Objects/content_screen.gd
@@ -2,41 +2,10 @@ extends Node2D
 var cell_image_width: float 
 var cell_image_height: float
 var speed: float = 500
-var curr_channel: String = "BPAE"
-var curr_button_value: String = "10x"
-
-var cell_images: Dictionary = {
-	"BPAE": {	
-		"10x": preload("res://Images/ImageCells/20250224_bpae_10xA1c1.jpg"),
-		"20x": preload("res://Images/ImageCells/20250224_bpae_20xA1c1.jpg"),
-		"40x": preload("res://Images/ImageCells/20250224_bpae_40xA1c1.jpg"),
-		"100x": preload("res://Images/ImageCells/20250224_bpae_100xA1c1.jpg")
-	},
-	"Dapi": {
-		"10x": preload("res://Images/ImageCells/ImageA1/Dapi/20250224_bpae_10xA1c1.jpg"),
-		"20x": preload("res://Images/ImageCells/ImageA1/Dapi/20250224_bpae_20xA1c1.jpg"),
-		"40x": preload("res://Images/ImageCells/ImageA1/Dapi/20250224_bpae_40xA1c1.jpg"),
-		"100x": preload("res://Images/ImageCells/ImageA1/Dapi/20250224_bpae_100xA1c1.jpg")
-	},
-	"FITC": {
-		"10x": preload("res://Images/ImageCells/ImageA1/FITC/20250224_bpae_10xA1c2.jpg"),
-		"20x": preload("res://Images/ImageCells/ImageA1/FITC/20250224_bpae_20xA1c2.jpg"),
-		"40x": preload("res://Images/ImageCells/ImageA1/FITC/20250224_bpae_40xA1c2.jpg"),
-		"100x": preload("res://Images/ImageCells/ImageA1/FITC/20250224_bpae_100xA1c2.jpg")
-	},
-	"TRITC": {
-		"10x": preload("res://Images/ImageCells/ImageA1/TRITC/20250224_bpae_10xA1c3.jpg"),
-		"20x": preload("res://Images/ImageCells/ImageA1/TRITC/20250224_bpae_20xA1c3.jpg"),
-		"40x": preload("res://Images/ImageCells/ImageA1/TRITC/20250224_bpae_40xA1c3.jpg"),
-		"100x": preload("res://Images/ImageCells/ImageA1/TRITC/20250224_bpae_100xA1c3.jpg")
-	},
-	"Cy5": {
-		"10x": preload("res://Images/ImageCells/ImageA1/Cy5/20250224_bpae_10xA1c4.jpg"),
-		"20x": preload("res://Images/ImageCells/ImageA1/Cy5/20250224_bpae_20xA1c4.jpg"),
-		"40x": preload("res://Images/ImageCells/ImageA1/Cy5/20250224_bpae_40xA1c4.jpg"),
-		"100x": preload("res://Images/ImageCells/ImageA1/Cy5/20250224_bpae_100xA1c4.jpg")
-	}
-}
+var curr_slide_type: String = "BPAE"
+var curr_slide_variation: String = "A1"
+var curr_channel: String = "DAPi"
+var curr_zoom_value: String = "10x"
 
 var direction: Vector2 = Vector2(0,0)
 func _ready() -> void:
@@ -67,10 +36,30 @@ func _process(delta: float) -> void:
 
 # Change Image Zoom
 func _on_macro_panel_button_press(button_value: String) -> void:
-	curr_button_value = button_value
-	$CellImage/Sprite2D.texture = cell_images[curr_channel][button_value]
+	curr_zoom_value = button_value
+	var image_path: String = "res://Images/ImageCells/{curr_slide_type}/{curr_slide_variation}/{curr_channel}/{curr_zoom_value}.jpg"
+	
+	#Godot doesn't have f strings, so using .format
+	image_path = image_path.format({
+		"curr_slide_type": curr_slide_type, 
+		"curr_slide_variation": curr_slide_variation, 
+		"curr_channel": curr_channel, 
+		"curr_zoom_value": curr_zoom_value
+	})
+
+	$CellImage/Sprite2D.texture = load(image_path)
 
 
 func _on_computer_channel_select(channel: String) -> void:
 	curr_channel = channel
-	$CellImage/Sprite2D.texture = cell_images[channel][curr_button_value]
+	var image_path: String = "res://Images/ImageCells/{curr_slide_type}/{curr_slide_variation}/{curr_channel}/{curr_zoom_value}.jpg"
+	
+	#Godot doesn't have f strings, so using .format
+	image_path = image_path.format({
+		"curr_slide_type": curr_slide_type, 
+		"curr_slide_variation": curr_slide_variation, 
+		"curr_channel": curr_channel, 
+		"curr_zoom_value": curr_zoom_value
+	})
+
+	$CellImage/Sprite2D.texture = load(image_path)
