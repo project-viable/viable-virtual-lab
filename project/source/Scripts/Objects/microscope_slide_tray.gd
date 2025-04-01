c0a4612c2e5ca15aa8ea8579a371fcdf6305a28f
Jackson Cockrum <jackson.cockrum@colorado.edu>
2025-04-01 13:46:09 -0600

    added in light switch functionality

--- project/source/Scripts/Objects/microscope_slide_tray.gd
+++ project/source/Scripts/Objects/microscope_slide_tray.gd
@@ -12,7 +12,7 @@ var light_on: bool = false
 
 func _ready() -> void:
 	$left_area.connect("input_event", _on_area_input.bind("left"))
-	$right_area.connect("input_event", _on_area_input.bind("slide_tray_right_openright"))
+	$right_area.connect("input_event", _on_area_input.bind("right"))
 
 func _on_area_input(_viewport: Viewport, event: InputEvent, _shape_idx: int, side: String) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
@@ -22,14 +22,10 @@ func _on_area_input(_viewport: Viewport, event: InputEvent, _shape_idx: int, sid
 			right_open = !right_open
 			
 		if right_open and left_open:
-			#TODO: make light turn on and off
-			#if light_on:
-				#texture = tray_open_light_off
-				#light_on = false
-			#else:
-				#texture = tray_open_light_on
-				#light_on = true
-			texture = tray_open_light_off
+			if light_on:
+				texture = tray_open_light_on
+			else:
+				texture = tray_open_light_off
 		elif right_open:
 			texture = slide_tray_right_open
 		elif left_open:
@@ -37,3 +33,13 @@ func _on_area_input(_viewport: Viewport, event: InputEvent, _shape_idx: int, sid
 		else:
 			texture = tray_closed
 				
+
+
+func _on_light_switch_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		light_on = !light_on
+		if right_open and left_open:
+			if light_on:
+				texture = tray_open_light_on
+			else:
+				texture = tray_open_light_off
