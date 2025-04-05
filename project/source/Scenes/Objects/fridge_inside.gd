52417891cd3f6f7d57d15c634d492e3056fac56e
Derick Sayavong <dsayavong54651@gmail.com>
2025-04-04 22:39:18 -0600

    Dragging the slides outside the fridge pop up and then exiting will keep the slides outside and vice versa

--- project/source/Scenes/Objects/fridge_inside.gd
+++ project/source/Scenes/Objects/fridge_inside.gd
@@ -2,24 +2,20 @@ extends Node2D
 
 var slide_script: Script = load("res://Scripts/draggable_microscope_slide.gd")
 var currently_dragging: bool = false
-var current_slide: StaticBody2D = null
+var current_slide: CharacterBody2D = null
 
 func _ready() -> void:
 	var slides: Array = get_tree().get_nodes_in_group("Slides")
-	for slide: StaticBody2D in slides:
-			
+	for slide: CharacterBody2D in slides:
 		slide.set_script(slide_script)
-		await get_tree().process_frame
+		slide.setup()
 		slide.set_process(true)
 		slide.is_selected.connect(_on_slide_is_selected)
-		slide.mouse_entered.connect(slide._on_mouse_entered)
-		slide.mouse_exited.connect(slide._on_mouse_exited)
-
 
 func _process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and current_slide and currently_dragging:
 		current_slide.global_position = get_global_mouse_position()
 
-func _on_slide_is_selected(slide: StaticBody2D, drag: bool) -> void:
+func _on_slide_is_selected(slide: CharacterBody2D, drag: bool) -> void:
 	current_slide = slide
 	currently_dragging = drag
--- project/source/Scripts/draggable_microscope_slide.gd
+++ project/source/Scripts/draggable_microscope_slide.gd
@@ -1,13 +1,38 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 signal is_selected
 var is_draggable: bool = false
+
+var is_inside_fridge: bool = true
+
+var scene_root: Node2D = get_tree().current_scene
+var original_parent: Node2D = null
+var current_parent: Node2D = null
+
+
+func setup() -> void:
+	original_parent = get_parent()
+	current_parent = original_parent
+	
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click") and is_draggable:
 		is_selected.emit(self, is_draggable)
 	elif Input.is_action_just_released("click"):
 		is_selected.emit(self, false)
+		
+		if not is_inside_fridge and current_parent == original_parent:
+			current_parent = scene_root
+			self.reparent(scene_root)
+		elif is_inside_fridge and current_parent != original_parent:
+			current_parent = original_parent
+			self.reparent(original_parent)
+		
+	
+		
+
 
 func _on_mouse_entered() -> void:
 	is_draggable = true
