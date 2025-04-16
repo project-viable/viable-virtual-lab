7a0f9ec996c7e51f59b41359ceb862936da28894
Jackson Cockrum <jackson.cockrum@colorado.edu>
2025-04-15 18:44:02 -0600

    added oil bottle to main scene

--- project/source/Scripts/Objects/zoom_oil.gd
+++ project/source/Scripts/Objects/zoom_oil.gd
@@ -1,11 +1,5 @@
 extends LabObject
+class_name ZoomOil
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func lab_object_ready() -> void:
+	draggable = true
