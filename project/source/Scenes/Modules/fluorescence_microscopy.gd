e89b65ffefb2c8a8c7ad4164d10a3229ccb61236
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-04-17 00:37:52 -0600

    Make the computer self-sufficient (ish)

--- project/source/Scenes/Modules/fluorescence_microscopy.gd
+++ project/source/Scenes/Modules/fluorescence_microscopy.gd
@@ -1,14 +1,5 @@
 extends Node2D
 
-func _ready() -> void:
-	$Border/focus_control.focus_changed.connect(_on_focus_control_focus_changed)
-
-func _on_computer_screen_click_signal() -> void:
-	get_node("Computer/PopupControl").visible = true
-	
-func _on_focus_control_focus_changed(level: float) -> void:
-	$Computer/PopupControl/PanelContainer/VBoxContainer/Screen/ContentScreen/CellImage/Sprite2D.material.set("shader_parameter/blur_amount", level)
-
 func _on_microscope_mount_slide(slide: DraggableMicroscopeSlide) -> void:
 	if slide and slide.slide_orientation_up:
 		$Computer.current_slide = slide.slide_name
