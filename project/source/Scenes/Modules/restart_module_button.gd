3671ad9424f396af85760bd880de6b320dbb56cd
jakobrosencu <jakob.rosen@colorado.edu>
2025-04-22 20:38:14 -0600

    removed restart module button in favor of Harper's PR

--- project/source/Scenes/Modules/restart_module_button.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _on_restart_module_button_pressed() -> void:
-	# looks gross... does the job
-	var menu: Node = $"../../../../../Menu"
-	if menu.current_module != null:
-		menu.module_selected(menu.current_module)
