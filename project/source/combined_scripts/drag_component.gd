4f49b55992d4aaba10f2cf562aea8a38b92a2939
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-07-10 17:24:51 -0600

    Fix mouse not staying in the same place on a sprite when it rotates while dragging
    This can be done by using the local mouse position as the drag offset
    instead of the global position.

--- project/source/combined_scripts/drag_component.gd
+++ project/source/combined_scripts/drag_component.gd
@@ -29,14 +29,14 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if _is_dragging:
-		body.global_position = get_global_mouse_position() + _offset
-
 		if abs(body.global_rotation) > 0.001:
 			var is_rotating_clockwise := body.global_rotation < 0
 			body.global_rotation -= body.global_rotation * delta * 50
 
 			if is_rotating_clockwise: body.global_rotation = min(0.0, body.global_rotation)
 			else: body.global_rotation = max(0.0, body.global_rotation)
+
+		body.global_position = body.to_global(body.get_local_mouse_position() - _offset)
 
 func _process(_delta: float) -> void:
 	_shader_mat.set(&"shader_parameter/enabled", _is_hovering_sprite() and not _is_dragging)
@@ -45,10 +45,10 @@ func _unhandled_input(e: InputEvent) -> void:
 	if e.is_action_pressed(&"DragLabObject") and _is_hovering_sprite():
 		_is_dragging = true
 		body.set_deferred(&"freeze", true)
-		_offset = body.global_position - get_global_mouse_position()
+		_offset = body.get_local_mouse_position()
 	elif e.is_action_released(&"DragLabObject") and _is_dragging:
 		_is_dragging = false
 		body.set_deferred(&"freeze", false)
 
 func _is_hovering_sprite() -> bool:
-	return interact_sprite.is_pixel_opaque(interact_sprite.to_local(get_global_mouse_position()))
+	return interact_sprite.is_pixel_opaque(interact_sprite.get_local_mouse_position())
