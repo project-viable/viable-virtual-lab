3fb8a4e34d7c18a322084bbcc17f5af53e12397b
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-07-10 13:56:21 -0600

    Add a drag component and flask

---
+++ project/source/combined_scripts/drag_component.gd
@@ -0,0 +1,48 @@
+## Allows a `RigidBody2D` to be dragged and dropped with the mouse.
+class_name DragComponent
+extends Node2D
+
+
+## The body to be dragged.
+@export var body: RigidBody2D
+
+## When the mouse is over this sprite, it will be highlighted, and pressing the
+## left mouse button while hovering it will allow it to be dragged.
+@export var interact_sprite: Sprite2D
+
+
+var _offset: Vector2 = Vector2.ZERO
+var _is_dragging: bool = false
+var _shader_mat: ShaderMaterial = ShaderMaterial.new()
+
+
+func _ready() -> void:
+	_shader_mat.shader = preload("res://shaders/outline.gdshader")
+	
+	var top_material := interact_sprite.material
+	while top_material:
+		top_material = top_material.next_pass
+
+	if top_material: top_material.next_pass = _shader_mat
+	else: interact_sprite.material = _shader_mat
+
+
+func _physics_process(_delta: float) -> void:
+	if _is_dragging:
+		body.global_position = get_global_mouse_position() + _offset
+
+func _process(_delta: float) -> void:
+	print(_is_hovering_sprite())
+	_shader_mat.set(&"shader_parameter/enabled", _is_hovering_sprite() and not _is_dragging)
+
+func _unhandled_input(e: InputEvent) -> void:
+	if e.is_action_pressed(&"DragLabObject") and _is_hovering_sprite():
+		_is_dragging = true
+		body.set_deferred(&"freeze", true)
+		_offset = body.global_position - get_global_mouse_position()
+	elif e.is_action_released(&"DragLabObject") and _is_dragging:
+		_is_dragging = false
+		body.set_deferred(&"freeze", false)
+
+func _is_hovering_sprite() -> bool:
+	return interact_sprite.is_pixel_opaque(interact_sprite.to_local(get_global_mouse_position()))
