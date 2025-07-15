345fb363d4746a4d750da8b18b38b11b3c13f4f2
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-07-14 18:04:15 -0600

    Account for z index when choosing which sprite to select

--- project/source/combined_scripts/selectables_singleton.gd
+++ project/source/combined_scripts/selectables_singleton.gd
@@ -14,6 +14,8 @@ var hovered_drag_component: DragComponent = null
 # idea that the order they call `_draw` would correspond with the which one is on top (if `_draw`
 # is called later, then it is on top).
 var _next_index: int = 0
+var _hovered_z_index: int = RenderingServer.CANVAS_ITEM_Z_MIN
+var _hovered_draw_order: int = 0
 
 
 func _draw() -> void:
@@ -21,13 +23,31 @@ func _draw() -> void:
 
 func _process(_delta: float) -> void:
 	hovered_drag_component = null
+	_hovered_z_index = RenderingServer.CANVAS_ITEM_Z_MIN
 	for c in get_tree().get_nodes_in_group(&"drag_component"):
-		if c is DragComponent \
-				and c.interact_canvas_group.is_mouse_hovering() \
-				and (not hovered_drag_component
-					or c.interact_canvas_group.draw_order_this_frame > hovered_drag_component.interact_canvas_group.draw_order_this_frame):
-			hovered_drag_component = c
+		if c is DragComponent:
+			var z := _get_absolute_z_index(c.interact_canvas_group)
+			var draw_order: int = c.interact_canvas_group.draw_order_this_frame
+
+			if c.interact_canvas_group.is_mouse_hovering() \
+					and (not hovered_drag_component
+						or z > _hovered_z_index
+						or draw_order > _hovered_draw_order and not z < _hovered_z_index):
+				hovered_drag_component = c
+				_hovered_z_index = z
+				_hovered_draw_order = draw_order
 
 func get_next_index() -> int:
 	_next_index += 1
 	return _next_index - 1
+
+# Don't know if this is 100% correct, but it works for now. Get the "absolute" z-index of a node,
+# taking into account relative z indices.
+static func _get_absolute_z_index(n: Node) -> int:
+	if not (n is CanvasItem):
+		return 0
+	elif n.z_as_relative:
+		return _get_absolute_z_index(n.get_parent()) + n.z_index
+	else:
+		return n.z_index
+
