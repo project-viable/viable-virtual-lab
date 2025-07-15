e08eaf1c9a773aab0081f8cdef0beb0194ef6ff4
Derick Sayavong <dsayavong54651@gmail.com>
2025-07-15 17:04:30 -0600

    Add GameState and Interaction Singleton

---
+++ project/source/combined_scripts/states.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var is_dragging: bool
+var interactor: PhysicsBody2D # The object being dragged on top of other objects
+var interactable: PhysicsBody2D # The object that is being interacted with
