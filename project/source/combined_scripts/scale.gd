e1e8915276a5d1b1762b773868396059b678949e
EveDiby <94596452+EveDiby@users.noreply.github.com>
2025-07-22 12:50:39 -0600

    Updated Taring Logic
    The taring logic should now work for cases in which a user were to click the tare but and add more substances to the weigh boat continously.

--- project/source/combined_scripts/scale.gd
+++ project/source/combined_scripts/scale.gd
@@ -5,28 +5,37 @@ var current_weight: float = 0.0 #in terms of grams
 
 func _physics_process(_delta: float) -> void:
 	
-	current_weight = 0.0
-	
 	for node: Node2D in $Area2D.get_overlapping_bodies():
-		if node is ContainerComponent:
-			current_weight += (node.get_container_mass() + node.get_substances_mass())
+		if node is ContainerComponent and node.name != "Scoopula":
+			current_weight = (node.get_container_mass() + node.get_substances_mass())
 
-	$CanvasLayer/Control/PanelContainer/VBoxContainer/Weight_Value.text = "%.2f" % [current_weight - tare_weight]
+	$CanvasLayer/Control/PanelContainer/VBoxContainer/Weight_Value.text = "%.2f" % [current_weight-tare_weight]
+	#print ("current_weight-tare_weight: ", current_weight-tare_weight)
 	
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	#We only care about weighing objects that can actually be weighed on a scale in real life.
 	#Having this if statement will ensure that when the scale is dragged around the screen it won't try to weigh a shelf or something.
-	if body is ContainerComponent:
+	if body is ContainerComponent and body.name != "Scoopula":
 		#The scale menu is under a canvas layer so that it sticks to the bottom left corner of the viewport when visible
+		set_deferred("freeze", true)
 		$CanvasLayer/Control.visible = true
-	else:
-		$CanvasLayer/Control.visible = false
+		
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	$CanvasLayer/Control.visible = false
-	tare_weight = 0.0
+	if body is ContainerComponent and body.name == "Scoopula":
+		for node: Node2D in $Area2D.get_overlapping_bodies():
+			if node is ContainerComponent and node.name != "Scoopula":
+				$CanvasLayer/Control.visible = true
+			else:
+				$CanvasLayer/Control.visible = false
+	else:
+		$CanvasLayer/Control.visible = false
+		tare_weight = 0.0
 	
 
 func _on_tare_button_pressed() -> void:
 	tare_weight = current_weight
+	print ("current_weight: ", current_weight)
+	print ("tare_weight: ", tare_weight)
+	
