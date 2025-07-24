d967fe7d7ba77d63df5fcdeb756c5914f78df170
Derick Sayavong <dsayavong54651@gmail.com>
2025-07-24 17:57:45 -0600

    Add pipette sprite without a tip and changed collision shape based on if it has a tip or not

---
+++ project/source/combined_scripts/pipette.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+class_name Pipe # TODO: Since the old sinmulaton uses a class_name Pipette already, had to change the name. Should name it Pipette when the old stuff is gone
+
+var has_tip: bool = false:
+	set(value):
+		has_tip = value
+		
+		# Change sprites and active collision depending if it has_tip is true or false
+		$CollisionPolygon2DNoTip.disabled = value
+		$SelectableCanvasGroup/PipetteNoTip.visible = !value
+		
+		$CollisionPolygon2DWithTip.disabled = !value
+		$SelectableCanvasGroup/PipetteWithTip.visible = value
