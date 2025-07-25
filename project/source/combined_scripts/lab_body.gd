738c665b6cc8f52914695c99fa615332b14d3b74
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-07-25 16:17:13 -0600

    Remove the SURFACE mode for LabBody in favor of using a child staticbody with one-way collision

--- project/source/combined_scripts/lab_body.gd
+++ project/source/combined_scripts/lab_body.gd
@@ -3,27 +3,12 @@ class_name LabBody
 extends RigidBody2D
 
 
-enum Mode
-{
-	NORMAL, # Affected by physics. Only collides with things in the `SURFACE` layer.
-	DRAGGING, # No collision nor physics; can be safely dragged around.
-	SURFACE, # Other objects can collide with this. Useful for objects like the scale that can have other objects sit on them.
-}
+func _ready() -> void: stop_dragging()
 
+func start_dragging() -> void:
+	set_deferred(&"collision_mask", 0)
+	set_deferred(&"freeze", true)
 
-@export var default_mode: Mode = Mode.NORMAL
-
-
-func _ready() -> void: set_mode(default_mode)
-
-func start_dragging() -> void: set_mode(Mode.DRAGGING)
-func stop_dragging() -> void: set_mode(default_mode)
-
-func set_mode(mode: Mode) -> void:
-	var layer: int = 1 if mode == Mode.SURFACE else 0
-	var mask: int = 0 if mode == Mode.DRAGGING else 1
-	var freeze: bool = mode == Mode.DRAGGING
-
-	set_deferred(&"collision_layer", layer)
-	set_deferred(&"collision_mask", mask)
-	set_deferred(&"freeze", freeze)
+func stop_dragging() -> void:
+	set_deferred(&"collision_mask", 1)
+	set_deferred(&"freeze", false)
