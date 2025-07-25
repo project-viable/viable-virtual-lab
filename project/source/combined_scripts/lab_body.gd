a8f3e6e407afab1db060c84fff6d32c62aac43b2
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-07-25 16:21:00 -0600

    Add a comment explaining the drag functions in LabBody

--- project/source/combined_scripts/lab_body.gd
+++ project/source/combined_scripts/lab_body.gd
@@ -5,6 +5,9 @@ extends RigidBody2D
 
 func _ready() -> void: stop_dragging()
 
+
+# `start_dragging` and `stop_dragging` don't actually handle any drag logic; they just change the
+# physics to allow for dragging.
 func start_dragging() -> void:
 	set_deferred(&"collision_mask", 0)
 	set_deferred(&"freeze", true)
