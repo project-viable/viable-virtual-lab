8f62edc32830b3afd456495c4f74c30fd0d6d794
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-08-11 17:03:17 -0600

    Add a basic "slot" interactable area and a version that allows an object to be attached via remote transform

---
+++ project/source/combined_scripts/attachment_interactable_area.gd
@@ -0,0 +1,43 @@
+class_name AttachmentInteractableArea
+extends ObjectSlotInteractableArea
+## If an object has a child of type `AttachmentPoint`, then it can be attached to this node such
+## that its attachment point is locked to the position of this node, held in place using a
+## `RemoteTransform2D`. This node should never be rotated nor scaled.
+
+
+@export var allowed_groups: Array[StringName] = []
+
+
+var _remote_transform := RemoteTransform2D.new()
+
+
+func _ready() -> void:
+	super()
+	call_deferred(&"add_child", _remote_transform)
+
+func _physics_process(_delta: float) -> void:
+	# The user started dragging the contained object, so we release it.
+	if Interaction.active_drag_component and Interaction.active_drag_component.body == contained_object:
+		remove_object()
+
+func can_place(body: LabBody) -> bool:
+	return _find_attachment_point(body) != null
+
+func on_place_object() -> void:
+	Interaction.active_drag_component.stop_dragging()
+	contained_object.start_dragging()
+
+	var ap := _find_attachment_point(contained_object)
+	if ap:
+		_remote_transform.position = -ap.position
+		_remote_transform.remote_path = contained_object.get_path()
+
+func on_remove_object() -> void:
+	_remote_transform.remote_path = NodePath()
+
+func _find_attachment_point(body: LabBody) -> AttachmentPoint:
+	for a: AttachmentPoint in body.find_children("", "AttachmentPoint", false):
+		if not allowed_groups or allowed_groups.any(func(g: StringName) -> bool: return a.is_in_group(g)):
+			return a
+	
+	return null
---
+++ project/source/combined_scripts/object_slot_interactable_area.gd
@@ -0,0 +1,58 @@
+class_name ObjectSlotInteractableArea
+extends InteractableArea
+## An interactable area that allows objects to be placed. The object doesn't actually get affected
+## when it's placed, but it's possible to add behavior for that in classes deriving from
+## `ObjectSlotInteractableArea`.
+
+
+signal object_placed(body: LabBody)
+signal object_removed(body: LabBody)
+
+
+@export var contained_object: LabBody = null
+@export var place_prompt: String = "Place"
+
+
+func get_interactions() -> Array[InteractInfo]:
+	if not contained_object and can_place(Interaction.active_drag_component.body):
+		return [InteractInfo.new(InteractInfo.Kind.PRIMARY, place_prompt)]
+	else:
+		return []
+
+func start_interact(_k: InteractInfo.Kind) -> void:
+	_place_object_unchecked(Interaction.active_drag_component.body)
+
+## (virtual) determine whether the given object can be placed in this slot. The object will be
+## placed only if this function returns true and there is not already a contained object.
+func can_place(_body: LabBody) -> bool: return false
+
+## (virtual) called when an object is placed. `contained_object` will always be set before this is
+## called.
+func on_place_object() -> void: pass
+
+## (virtual) called when an object is removed. `contained_object` is set to null only *after* this
+## function returns, so it can be accessed there.
+func on_remove_object() -> void: pass
+
+## Call this to attempt to place an object. Returns true if the object was placed.
+func place_object(body: LabBody) -> bool:
+	if not contained_object and can_place(Interaction.active_drag_component.body):
+		_place_object_unchecked(body)
+		return true
+
+	return false
+
+## Call this to remove the current object, if it is there. Returns true if an object was removed.
+func remove_object() -> bool:
+	if contained_object:
+		on_remove_object()
+		object_removed.emit(contained_object)
+		contained_object = null
+		return true
+
+	return false
+
+func _place_object_unchecked(body: LabBody) -> void:
+	contained_object = body
+	on_place_object()
+	object_placed.emit(body)
