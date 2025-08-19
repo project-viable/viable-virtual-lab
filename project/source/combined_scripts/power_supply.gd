e6a8a6aa6ec56c01a2d7f7ade6fcb8efdd4ae9ad
Derick Sayavong <dsayavong54651@gmail.com>
2025-08-19 16:03:00 -0600

    Add time and voltage to power supply

--- project/source/combined_scripts/power_supply.gd
+++ project/source/combined_scripts/power_supply.gd
@@ -6,16 +6,61 @@ class_name PowerSupply
 # the experiment will run just fine
 @export var positive_connected: bool = false
 @export var negative_connected: bool = false
+@export var increment_time_button: TextureButton
+@export var decrement_time_button: TextureButton
+@export var increment_volts_button: TextureButton
+@export var decrement_volts_button: TextureButton
+
+@onready var button_function_dict: Dictionary = {
+	increment_time_button: {
+		"function": increment_time,
+		"type": ConfigType.TIME,
+	},
+	decrement_time_button: {
+		"function": decrement_time,
+		"type": ConfigType.TIME,
+	},
+	increment_volts_button: {
+		"function": increment_volts,
+		"type": ConfigType.VOLT,
+	},
+	decrement_volts_button: {
+		"function": decrement_volts,
+		"type": ConfigType.VOLT,
+	},
+}
 
 signal activate_power_supply(voltage: float, time: int, is_circuit_ready: bool)
 
-var wire_connected_to_positive_output: Wire
-var wire_connected_to_negative_output: Wire
+enum ConfigType{
+	TIME,
+	VOLT
+}
+
+var _wire_connected_to_positive_output: Wire
+var _wire_connected_to_negative_output: Wire
+var _is_zoomed_in: bool = false
+var _should_increment: bool = false
+var _time: int = 0 # Time in seconds
+var _volts: int = 50 
+var _buttons: Array[TextureButton]
+
+var _current_pressed_button: TextureButton
+var _delta_time: int = 1
+var _delta_volts: int = 1
+var _wait_time_threshold: float = .05
+
+func _ready() -> void:
+	$Screen/VoltageContainer/HBoxContainer/Volts.text = "%d" % [_volts]
+	Engine.max_fps = 60
+	for button: TextureButton in find_children("", "TextureButton", true):
+		_buttons.append(button)
+		button.button_down.connect(_on_screen_button_pressed.bind(button))
+		button.button_up.connect(_on_screen_button_released.bind(button))
 
 func _on_start_button_pressed() -> void:
 	var circuit_ready: bool = positive_connected and negative_connected
-	activate_power_supply.emit(180.0, 30, circuit_ready)
-
+	activate_power_supply.emit(_volts, _time, circuit_ready) #TODO stuff should happen once wires are connected to the gel rig
 
 func _on_wire_connected(wire: Wire, target_outlet_charge: Wire.Charge) -> void:
 	var is_charge_matching: bool = wire.charge == target_outlet_charge
@@ -24,31 +69,130 @@ func _on_wire_connected(wire: Wire, target_outlet_charge: Wire.Charge) -> void:
 	if is_charge_matching:
 		if is_outlet_positive:
 			positive_connected = true
-			wire_connected_to_positive_output = wire
+			_wire_connected_to_positive_output = wire
 			
 		else:
 			negative_connected = true
-			wire_connected_to_negative_output = wire
+			_wire_connected_to_negative_output = wire
 	
 	else:
 		if is_outlet_positive:
 			print("Connecting a Negative to a Positive!")
-			wire_connected_to_positive_output = wire
+			_wire_connected_to_positive_output = wire
 			
 		else:
 			print("Connecting a Positive to a Negative!")
-			wire_connected_to_negative_output = wire
+			_wire_connected_to_negative_output = wire
 
 ## Handle unplugging wires from the Power Supply
-func unplug_handler(body: Node2D) -> void:
+func _unplug_handler(body: Node2D) -> void:
 	var clicked_on_wire: Wire = body
 	
-	if wire_connected_to_positive_output and clicked_on_wire == wire_connected_to_positive_output: # Pulling out the wire from positive outlet
+	if _wire_connected_to_positive_output and clicked_on_wire == _wire_connected_to_positive_output: # Pulling out the wire from positive outlet
 		positive_connected = false
-		wire_connected_to_positive_output = null
+		_wire_connected_to_positive_output = null
 		
 		
-	elif wire_connected_to_negative_output and clicked_on_wire == wire_connected_to_negative_output: # Pulling out the wire from negative outlet
+	elif _wire_connected_to_negative_output and clicked_on_wire == _wire_connected_to_negative_output: # Pulling out the wire from negative outlet
 		negative_connected = false
-		wire_connected_to_negative_output = null
+		_wire_connected_to_negative_output = null
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ExitCameraZoom"):
+		_is_zoomed_in = false
+		for button in _buttons:
+			button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+func _on_screen_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_pressed():
+		TransitionCamera.target_camera = $ZoomCamera
+		_is_zoomed_in = true
+		for button in _buttons:
+			button.mouse_filter = Control.MOUSE_FILTER_STOP
+
+func _on_screen_mouse_entered() -> void:
+	$DragComponent.enable_interaction = false
+
+func _on_screen_mouse_exited() -> void:
+	if not _is_zoomed_in:
+		$DragComponent.enable_interaction = true
+ 
+func _on_timer_timeout() -> void:
+	var is_pressed: bool = _current_pressed_button.button_pressed
+	var button_function: Callable = button_function_dict[_current_pressed_button]["function"]
+	var config_type: ConfigType = button_function_dict[_current_pressed_button]["type"]
+	
+	if is_pressed and config_type == ConfigType.TIME:
+		gradually_change(button_function, config_type, 60)
+
+	elif is_pressed and config_type == ConfigType.VOLT:
+		gradually_change(button_function, config_type, 50)
 		
+func _on_screen_button_pressed(button: TextureButton) -> void:
+	_current_pressed_button = button
+	button_function_dict[_current_pressed_button]["function"].call() # Call once for single clicks
+	$Timer.start()
+
+func _on_screen_button_released(button: TextureButton) -> void:
+	$Timer.stop()
+	$Timer.wait_time = 0.15
+	_delta_time = 1
+	_delta_volts = 1
+	_should_increment = false
+	
+func increment_time() -> int:
+	_time += _delta_time
+	update_timer_display()
+	return _time
+	
+func decrement_time() -> int:
+	if _time > 0:
+		_time -= _delta_time
+		update_timer_display()
+	
+	return _time
+		
+func update_timer_display() -> void:
+	var minutes: int = _time / 60
+	var seconds: int = _time % 60
+	$Screen/TimeContainer/HBoxContainer/Time.text = "%d:%02d" % [minutes, seconds]
+
+func increment_volts() -> int:
+	_volts += _delta_volts
+	_volts = min(_volts, 999)
+	_update_volt_display()
+	
+	return _volts
+	
+func decrement_volts() -> int:
+	if _volts > 0:
+		_volts -= _delta_volts
+		_update_volt_display()
+	
+	return _volts
+	
+func _update_volt_display() -> void:
+	$Screen/VoltageContainer/HBoxContainer/Volts.text = "%d" % [_volts]
+
+func gradually_change(button_func: Callable, type: ConfigType, increment_value: int) -> void:
+	# Get either _time or _volts
+	var target_var_value: int = button_func.call()
+	
+	# Decrease the Timer's wait time to gradually increase the speed of changing values
+	if (target_var_value % increment_value != 0 or $Timer.wait_time > _wait_time_threshold) and not _should_increment:
+		$Timer.wait_time = max($Timer.wait_time - 0.0025, .05)
+	
+	# Start incrementing the value by a set amount
+	else:
+		_should_increment = true
+		$Timer.wait_time = .3 # Values change more slowly
+		$Timer.start() # Timer needs to start again to update the wait_time
+		change_delta_rate(type, increment_value)
+		
+## Updates the delta for time or volts to a set increment
+func change_delta_rate(type: ConfigType, new_delta: int) -> void:
+	if type == ConfigType.TIME:
+		_delta_time = new_delta 
+		
+	elif type == ConfigType.VOLT:
+		_delta_volts = new_delta
