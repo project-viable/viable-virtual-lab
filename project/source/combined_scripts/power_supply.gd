d0aabcb824cb9f099eb42bd4dfc0c2b894bf1c19
Derick Sayavong <dsayavong54651@gmail.com>
2025-08-20 19:14:38 -0600

    Users can type in time and voltage

--- project/source/combined_scripts/power_supply.gd
+++ project/source/combined_scripts/power_supply.gd
@@ -10,6 +10,8 @@ class_name PowerSupply
 @export var decrement_time_button: TextureButton
 @export var increment_volts_button: TextureButton
 @export var decrement_volts_button: TextureButton
+@export var time_line_edit: LineEdit
+@export var voltage_line_edit: LineEdit
 
 @onready var button_function_dict: Dictionary = {
 	increment_time_button: {
@@ -48,6 +50,7 @@ var _time: int = 0 # Time in seconds
 var _volts: int = 50 
 var _delta_time: int = 1
 var _delta_volts: int = 1
+var time_string_input: String = ""
 
 # How much it should increment by if the button is held down long enough
 var _time_increment: int = 60
@@ -56,7 +59,7 @@ var _volts_increment: int = 50
 var _wait_time_threshold: float = .05
 
 func _ready() -> void:
-	$Screen/VoltageContainer/HBoxContainer/Volts.text = "%d" % [_volts]
+	voltage_line_edit.text = "%d" % [_volts]
 	for button: TextureButton in find_children("", "TextureButton", true):
 		_buttons.append(button)
 		button.button_down.connect(_on_screen_button_pressed.bind(button))
@@ -106,14 +109,24 @@ func _input(event: InputEvent) -> void:
 		_is_zoomed_in = false
 		for button in _buttons:
 			button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-
+		
+		time_line_edit.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		voltage_line_edit.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		
+	if event.is_action_pressed("ui_text_backspace") and time_line_edit.has_focus():
+		time_string_input = time_string_input.left(-1)
+		update_timer_display()
+		
 func _on_screen_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_pressed():
 		TransitionCamera.target_camera = $ZoomCamera
 		_is_zoomed_in = true
 		for button in _buttons:
 			button.mouse_filter = Control.MOUSE_FILTER_STOP
-
+		
+		time_line_edit.mouse_filter = Control.MOUSE_FILTER_STOP
+		voltage_line_edit.mouse_filter = Control.MOUSE_FILTER_STOP
+		
 func _on_screen_mouse_entered() -> void:
 	$DragComponent.enable_interaction = false
 
@@ -136,6 +149,8 @@ func _on_screen_button_pressed(button: TextureButton) -> void:
 	_current_pressed_button = button
 	button_function_dict[_current_pressed_button]["function"].call() # Call once for single clicks
 	$Timer.start()
+	
+		
 
 func _on_screen_button_released(button: TextureButton) -> void:
 	$Timer.stop()
@@ -181,9 +196,21 @@ func decrement_time() -> int:
 	return _time
 		
 func update_timer_display() -> void:
-	var minutes: int = _time / 60
-	var seconds: int = _time % 60
-	$Screen/TimeContainer/HBoxContainer/Time.text = "%d:%02d" % [minutes, seconds]
+	var minutes: int
+	var seconds: int
+	
+	# If a user is typing in a number like 1230, convert it to 12:30 and update _time in seconds
+	if time_line_edit.has_focus():
+		var temp_time: int = int(time_string_input)
+		minutes = temp_time / 100
+		seconds = temp_time % 100
+		_time = minutes * 60 + seconds
+		
+	else: # Button inputs
+		minutes = _time / 60
+		seconds = _time % 60
+	
+	$Screen/TimeContainer/HBoxContainer/Time.text = "%02d:%02d" % [minutes, seconds]
 
 func increment_volts() -> int:
 	_volts += _delta_volts
@@ -200,4 +227,40 @@ func decrement_volts() -> int:
 	return _volts
 	
 func _update_volt_display() -> void:
-	$Screen/VoltageContainer/HBoxContainer/Volts.text = "%d" % [_volts]
+	voltage_line_edit.text = "%d" % [_volts]
+
+## Basically just using this to get number input
+func _on_time_line_edit_text_changed(new_text: String) -> void:
+	var line_edit: LineEdit = time_line_edit
+	if time_string_input.length() < 4 and new_text.is_valid_int():
+		time_string_input += new_text
+	
+	line_edit.text = ""
+	update_timer_display()
+
+
+func _on_line_edit_focus_exited() -> void:
+	# Since time is in seconds, the string should be what's shown exactly on the screen
+	# In this case the minutes and seconds concatenated to one another
+	time_string_input = str(_time / 60) + str(_time % 60)
+
+
+func _on_voltage_line_edit_text_changed(new_text: String) -> void:
+	var column: int = voltage_line_edit.caret_column
+	var filtered_text: String = ""
+	
+	var regex: RegEx = RegEx.new()
+	regex.compile("\\d+")
+	
+	var numbers: Array[RegExMatch] = regex.search_all(new_text)
+	
+	for number in numbers:
+		filtered_text += str(number.get_string())
+	
+	voltage_line_edit.text = filtered_text
+	voltage_line_edit.caret_column = column
+	_volts = int(voltage_line_edit.text)
+
+func _on_voltage_line_edit_focus_exited() -> void:
+	if voltage_line_edit.text.is_empty():
+		_update_volt_display()
