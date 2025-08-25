ec81cefaf200a74543c3d2d67dfca90487f53633
Derick Sayavong <dsayavong54651@gmail.com>
2025-08-25 12:08:23 -0600

    Account for invalid input on power supply

--- project/source/combined_scripts/voltage_line_edit.gd
+++ project/source/combined_scripts/voltage_line_edit.gd
@@ -8,9 +8,9 @@ func _on_voltage_line_edit_text_changed(new_text: String) -> void:
 	for char in new_text:
 		if char.is_valid_int():
 			filtered_text += char
+		else: # Keep the caret in the same place if invalid int is inputted
+			column -= 1
 
-		else:
-			column -= 1	
 	text = filtered_text
 	caret_column = column
 	power_supply.volts = int(text)
