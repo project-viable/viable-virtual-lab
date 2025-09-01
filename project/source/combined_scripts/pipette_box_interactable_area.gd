20e6d54eb4f00557e9152cf4cfc67b8c1aa361db
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-09-01 17:56:13 -0600

    Improve wording of the disabled pipette box prompt

--- project/source/combined_scripts/pipette_box_interactable_area.gd
+++ project/source/combined_scripts/pipette_box_interactable_area.gd
@@ -1,7 +1,7 @@
 extends InteractableArea
 
 var _info: InteractInfo = InteractInfo.new(InteractInfo.Kind.PRIMARY, "Add Tip")
-var _disallowed_info: InteractInfo = InteractInfo.new(InteractInfo.Kind.PRIMARY, "(Tip is already attached)", false)
+var _disallowed_info: InteractInfo = InteractInfo.new(InteractInfo.Kind.PRIMARY, "Add Tip (Pipette already has a tip)", false)
 
 func get_interactions() -> Array[InteractInfo]:
 	if Interaction.held_body is Pipe:
@@ -12,9 +12,5 @@ func get_interactions() -> Array[InteractInfo]:
 
 func start_interact(_kind: InteractInfo.Kind) -> void:
 	var pipette: Pipe = Interaction.held_body
-	if pipette.has_tip:
-		print("Pipette already has a tip!")
-		return
-	
 	pipette.has_tip = true
 	pipette.is_tip_contaminated = false
