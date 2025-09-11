0c58d1ba5bb0f6384a08911fe5376556ba46d9b9
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-09-11 15:35:21 -0600

    Remove crappy prototype zoom box thing

--- project/source/combined_scripts/pipette.gd
+++ project/source/combined_scripts/pipette.gd
@@ -21,9 +21,3 @@ func _on_use_component_volume_changed() -> void:
 
 func _on_popup_timer_timeout() -> void:
 	$Panel.hide()
-
-func _process(_delta: float) -> void:
-	$ZoomPanelContainer.hide()
-	for b: Node2D in $Area2D.get_overlapping_bodies():
-		if b is GelMold:
-			$ZoomPanelContainer.show()
