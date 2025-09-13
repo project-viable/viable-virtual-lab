4b50cd82060cdd07486e9f0bdeefbea340ed2bfd
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-09-12 19:19:39 -0600

    Add something similar to SubViewportContainer that scales the resulting rendered thing

---
+++ project/source/combined_scripts/content_scaled_subviewport_container.gd
@@ -0,0 +1,37 @@
+class_name ContentScaledSubViewportContainer
+extends TextureRect
+## Acts like a stripped-down version of [SubViewportContainer], but respects
+## [constant Window.CONTENT_SCALE_MODE_CANVAS_ITEMS]
+##
+## Normally, [SubViewportContainer] displays the content of a [SubViewport] at exactly that
+## viewport's size. However, sometimes, we want to render at a higher resolution and scale down.
+## This handles that.
+
+
+@onready var _viewport: SubViewport = Util.find_child_of_type(self, SubViewport)
+
+
+func _ready() -> void:
+	expand_mode = EXPAND_IGNORE_SIZE
+	texture = _viewport.get_texture()
+
+func _input(e: InputEvent) -> void:
+	_propagate_nonpositional_event(e)
+
+func _unhandled_input(e: InputEvent) -> void:
+	_propagate_nonpositional_event(e)
+
+func _gui_input(e: InputEvent) -> void:
+	if _is_propagated_in_gui_input(e):
+		_send_event_to_viewport(e.xformed_by(Transform2D.IDENTITY.scaled((_viewport.size as Vector2) / size)))
+
+func _propagate_nonpositional_event(e: InputEvent) -> void:
+	if not _is_propagated_in_gui_input(e):
+		_send_event_to_viewport(e)
+
+func _is_propagated_in_gui_input(e: InputEvent) -> bool:
+	return e is InputEventMouse or e is InputEventScreenDrag or e is InputEventScreenTouch or e is InputEventGesture
+
+func _send_event_to_viewport(e: InputEvent) -> void:
+	if _viewport and not _viewport.is_input_disabled():
+		_viewport.push_input(e)
