defbac9701caf5253345275e1cae21b1bf9b1a11
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-09-22 16:00:33 -0600

    Include the weight of every direct ContainerComponent child when weighing on the scale
    Some objects, like the gel mold, can have multiple `ContainerComponent`s
    that should all probably be accounted for when taking their weight.
    Doing it this way also ensures that we don't have to worry about
    ordering `ContainerComponent`s in an object to make sure that the first
    one is the "correct" one.
    
    This *still* doesn't properly handle more complex weighing situations,
    like when an object is attached to the object being weighed. I'm not
    sure how to best handle this here, but if we had interfaces, then I
    would probably make some sort of `Weighable` interface that the
    `AttachmentInteractableArea`s could then implement.

--- project/source/combined_scripts/scale_interactable_area.gd
+++ project/source/combined_scripts/scale_interactable_area.gd
@@ -10,21 +10,20 @@ func _physics_process(_delta: float) -> void:
 	## The scale will constantly be waiting to update the display of grams 
 	current_weight = 0.0
 	for body:LabBody in get_parent().find_child("InteractableArea").get_overlapping_bodies():
-		if body is LabBody and not body.freeze:
-			current_weight += (body.find_child("ContainerComponent", false).container_mass + body.find_child("ContainerComponent", false).get_substances_mass())
-			#print ("get_substances_mass: ", body.find_child("ContainerComponent").get_substances_mass())
-			#print ("container_mass: ", body.find_child("ContainerComponent").container_mass)
-			
+		# Don't weigh objects unless they are affected by gravity.
+		if not (body is LabBody) or body.freeze: continue
+
+		for c: ContainerComponent in body.find_children("", "ContainerComponent", false):
+			current_weight += (c.container_mass + c.get_substances_mass())
+
 	get_parent().find_child("WeightLabel").text = "%.2f" % [current_weight-tare_weight]
-	#print("tare_weight: ", tare_weight)
-	#print ("current_weight-tare_weight: ", current_weight-tare_weight)
-	
+
 ## Handles when the area is clicked on. If so zoom in on the scale
 func _on_scale_use_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("click") and not is_zoomed_in:
 		is_zoomed_in = true
 		TransitionCamera.target_camera = camera
-		
+
 ## Handles when the user wants to zoom back out to the main scene
 func _input(event: InputEvent) -> void:
 	if is_zoomed_in and event.is_action_pressed("ExitCameraZoom"):
