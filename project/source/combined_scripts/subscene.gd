fb3043301fcce4c0df5ddadd2f37d6a7f9431025
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-09-24 17:12:27 -0600

    Add basic subscene and subscene camera code

---
+++ project/source/combined_scripts/subscene.gd
@@ -0,0 +1,20 @@
+@tool
+class_name Subscene
+extends ReferenceRect
+## Rectangular region that will automatically be moved to a free space in the world not overlapping
+## any other subscene. Subscenes can be used to make a secondary, zoomed-in view of an object, and
+## can be displayed using a [SubsceneCamera].
+
+
+# Free space dedicated to this object. This is set when allocating this subscene.
+var default_position: Vector2 = Vector2.ZERO
+
+
+func _enter_tree() -> void:
+	top_level = true
+	if Engine.is_editor_hint():
+		if get_tree().edited_scene_root != owner:
+			hide()
+	else:
+		#Subscenes.allocate(self)
+		pass
---
+++ project/source/combined_scripts/subscene_camera.gd
@@ -0,0 +1,38 @@
+@tool
+class_name SubsceneCamera
+extends Camera2D
+## Provides a view of part of a subscene.
+##
+## Since this is shown in a box in  the middle of the
+## screen rather than filling the whole viewport, it can't just be enabled using
+## [method Camera2D.make_current]; instead, it must be enabled with the [Subscenes] singleton.
+##
+## Unlike normal cameras, which determine their size based on their viewport, a [SubsceneCamera]
+## actually dictates the size of the viewport. This means that you can't set the scale directly;
+## instead, you should set the [member display_size] variable.
+
+
+## Size of the region displayed by this camera, in local coordinates. The region will always be
+## centered on the position of this camera.
+@export var region_size: Vector2 = Vector2(100, 100) :
+	set(v):
+		region_size = v
+		if Engine.is_editor_hint(): queue_redraw()
+
+
+var _last_offset := offset
+
+
+func _enter_tree() -> void:
+	# The default (main) viewport is not the one that will be used by this camera, so the display
+	# bounds shown will be incorrect. This means we have to disable the default bounds rectangle
+	# and draw our own.
+	editor_draw_screen = false
+
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		draw_rect(Rect2(-region_size / 2 + offset, region_size), Color.RED, false)
+
+		if _last_offset != offset:
+			queue_redraw()
+			_last_offset = offset
