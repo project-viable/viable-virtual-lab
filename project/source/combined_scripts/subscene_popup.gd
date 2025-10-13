57d72732139614e0f3a6310914d5bd866683e4b8
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-10-13 16:50:57 -0600

    Improve subscene popup visibility and add exported camera variable

--- project/source/combined_scripts/subscene_popup.gd
+++ project/source/combined_scripts/subscene_popup.gd
@@ -7,32 +7,36 @@ extends Node2D
 ## the screen on that side, then it will be put on a different side.
 @export var side_hint: Side = SIDE_TOP
 @export var popup_margin: float = 30.0
+@export var subscene_camera: SubsceneCamera
 
 
 func _ready() -> void:
 	$CanvasLayer.custom_viewport = Subscenes.top_level_viewport
 	$%SubViewport.world_2d = Subscenes.main_world_2d
+	hide()
+	$%SubViewportContainer.hide()
 
-func start_displaying(camera: SubsceneCamera) -> void:
-	if not camera: return
-	camera.custom_viewport = $%SubViewport
-	$%SubViewportContainer.size = camera.region_size
-	camera.make_current()
+func start_displaying() -> void:
+	if not subscene_camera: return
+	subscene_camera.custom_viewport = $%SubViewport
+	$%SubViewportContainer.size = subscene_camera.region_size
+	subscene_camera.make_current()
 
 	# Get the position of this node with respect to the subscene canvas layer.
 	var screen_pos: Vector2 = $CanvasLayer.get_final_transform().affine_inverse() \
 			* get_global_transform_with_canvas().get_origin()
-	var offset := -camera.region_size / 2.0
+	var offset := -subscene_camera.region_size / 2.0
 
 	match side_hint:
-		SIDE_TOP: offset.y = -camera.region_size.y - popup_margin
+		SIDE_TOP: offset.y = -subscene_camera.region_size.y - popup_margin
 		SIDE_BOTTOM: offset.y = popup_margin
 		SIDE_LEFT: offset.x = popup_margin
-		SIDE_RIGHT: offset.x = -camera.region_size.x - popup_margin
+		SIDE_RIGHT: offset.x = -subscene_camera.region_size.x - popup_margin
 
 	$%SubViewportContainer.global_position = screen_pos + offset
-
 	show()
+	$%SubViewportContainer.show()
 
 func stop_displaying() -> void:
 	hide()
+	$%SubViewportContainer.hide()
