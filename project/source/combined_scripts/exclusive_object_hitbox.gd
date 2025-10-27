fb7f40916a1c2fdc1a2dfa6c2047ea921f48e996
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-10-27 17:47:31 -0600

    Add some virtual functions to `ExclusiveObjectHitbox` to make it a bit more flexible

--- project/source/combined_scripts/exclusive_object_hitbox.gd
+++ project/source/combined_scripts/exclusive_object_hitbox.gd
@@ -13,7 +13,7 @@ var _area_stack: Array[ExclusiveArea2D] = []
 func enter_area(area: ExclusiveArea2D) -> void:
 	var prev_area := get_current_area()
 	# We can't enter an area while we're already in it.
-	if not _area_stack.has(area):
+	if _can_enter_purview_of(area) and not _area_stack.has(area):
 		_area_stack.push_back(area)
 	_update_purview(prev_area)
 
@@ -26,13 +26,25 @@ func get_current_area() -> ExclusiveArea2D:
 	if _area_stack: return _area_stack.back()
 	else: return null
 
+## (virtual) This object will only enter the purview of [param _area] if this returns
+## [code]true[/code]. This can be overridden to only allow specific areas.
+func _can_enter_purview_of(_area: ExclusiveArea2D) -> bool: return true
+## (virtual) Called when this object enters the purview of the area [param _area]. This is always called
+## [i]before[/i] [signal entered_purview_of] is emitted.
+func _enter_purview_of(_area: ExclusiveArea2D) -> void: pass
+## (virtual) Called when this object leaves the purview of the area [param _area]. This is called [i]after[/i]
+## [signal left_purview_of] is emitted.
+func _leave_purview_of(_area: ExclusiveArea2D) -> void: pass
+
 func _update_purview(prev_area: ExclusiveArea2D) -> void:
 	var cur_area := get_current_area()
 	if prev_area != cur_area:
 		if prev_area:
 			prev_area.object_leave_purview(self)
 			left_purview_of.emit(prev_area)
+			_leave_purview_of(prev_area)
 		if cur_area:
+			_enter_purview_of(cur_area)
 			cur_area.object_enter_purview(self)
 			entered_purview_of.emit(cur_area)
 
