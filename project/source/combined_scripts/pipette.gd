4e24e5cd5dfbd708f801ca6a89a8dd070e726c1b
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-10-28 01:02:39 -0600

    Add subscene support to the pipette

--- project/source/combined_scripts/pipette.gd
+++ project/source/combined_scripts/pipette.gd
@@ -16,14 +16,40 @@ const VOLUME_PER_DIST: float = PLUNGE_VOLUME / PLUNGER_OFFSET
 		$SelectableCanvasGroup/PipetteWithTip.visible = has_tip
 		$SelectableCanvasGroup/SubstanceDisplayPolygon.visible = has_tip
 		$TipCollision.disabled = not has_tip
+
+		$%SubsceneTip.visible = has_tip
+		$%SubsceneTipCollision.disabled = not has_tip
+		$%SubsceneSubstanceDisplay.visible = has_tip
+
 		$ExclusiveObjectHitbox/NoTipEnd.disabled = has_tip
 		$ExclusiveObjectHitbox/WithTipEnd.disabled = not has_tip
 
 @onready var _orig_plunger_pos: float = $%Plunger.position.y
+@onready var _orig_sub_pipette_pos: Vector2 = $%SubscenePipette.position
+var _cur_subscene_camera: SubsceneCamera = null
+var _mouse_movement := Vector2.ZERO
+var _mouse_pos_before_zoom := Vector2.ZERO
 
+
+func _input(e: InputEvent) -> void:
+	if e is InputEventMouseMotion:
+		_mouse_movement += e.relative
+		print(_mouse_movement)
+
+func _process(_delta: float) -> void:
+	# Stop looking at the subscene if the pipette has been removed.
+	if _cur_subscene_camera and not Util.get_camera_world_rect(_cur_subscene_camera).has_point($%SubsceneTipOpeningArea.global_position):
+		_leave_subscene()
 
 func _physics_process(delta: float) -> void:
 	super(delta)
+
+	# Handle movement with subscene
+	if _cur_subscene_camera:
+		$%SubscenePipette.move_and_collide(_mouse_movement)
+
+	_mouse_movement = Vector2.ZERO
+
 	var plunge_diff: float = 0
 	# Very very lazy way to do this. This should use the interaction system.
 	if Input.is_action_pressed("interact_secondary"):
@@ -35,14 +61,22 @@ func _physics_process(delta: float) -> void:
 	$%Plunger.position.y += plunge_diff
 
 	var substance_display: SubstanceDisplayPolygon = null
-	for a: Area2D in $TipOpeningArea.get_overlapping_areas():
-		if a is SubstanceAccessArea:
-			substance_display = a.substance_display
-			break
+	# If we are zoomed in with a subscene, we want to pipette into the subscene instead.
+	if _cur_subscene_camera:
+		for a: Area2D in $%SubsceneTipOpeningArea.get_overlapping_areas():
+			if a is SubstanceAccessArea:
+				substance_display = a.substance_display
+				break
+	else:
+		for a: Area2D in $TipOpeningArea.get_overlapping_areas():
+			if a is SubstanceAccessArea:
+				substance_display = a.substance_display
+				break
 
 	if substance_display and has_tip:
+		var tip_node: Node2D = $%SubsceneTipOpeningArea if _cur_subscene_camera else $TipOpeningArea
 		# Only pull if we're below the fluid level.
-		if plunge_diff < -0.001 and substance_display.global_fluid_top_y_coord <= $TipOpeningArea.global_position.y:
+		if plunge_diff < -0.001 and substance_display.global_fluid_top_y_coord <= tip_node.global_position.y:
 			var volume: float = -plunge_diff * VOLUME_PER_DIST
 			$ContainerComponent.add(substance_display.source.take_volume(volume))
 		elif plunge_diff > 0.001:
@@ -56,9 +90,28 @@ func _on_exclusive_object_hitbox_entered_purview_of(area: ExclusiveArea2D) -> vo
 	if object_to_zoom and is_active():
 		# Zoom in on the object, with extra room above it for this pipette.
 		var rect := Util.get_global_bounding_box(object_to_zoom).grow_side(1, Util.get_global_bounding_box(self).size.y + 5)
-		Game.main.focus_camera_on_rect(rect, 1.5)
+		
+		if area is SubsceneExclusiveArea:
+			_cur_subscene_camera = area.camera
+			disable_follow_cursor = true
+			disable_drop = true
+			Game.main.focus_camera_and_show_subscene(rect, _cur_subscene_camera, true, 1.5)
+			$%SubscenePipette.global_position = area.entry_node.global_position - $%SubsceneTipOpeningArea.position
+			_mouse_pos_before_zoom = Cursor.virtual_mouse_position
+		else:
+			Game.main.focus_camera_on_rect(rect, 1.5)
 
 func _on_exclusive_object_hitbox_left_purview_of(area: ExclusiveArea2D) -> void:
 	collision_mask &= ~0b1000
 	z_index = 0
+	_leave_subscene()
 	Game.camera.return_to_main_scene(1.5)
+
+func _leave_subscene() -> void:
+	if _cur_subscene_camera:
+		Game.main.hide_subscene()
+		disable_follow_cursor = false
+		disable_drop = false
+		$%SubscenePipette.position = _orig_sub_pipette_pos
+		Cursor.virtual_mouse_position = _mouse_pos_before_zoom
+		_cur_subscene_camera = null
