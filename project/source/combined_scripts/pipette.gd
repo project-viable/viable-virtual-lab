2b528d919319399f380c4c21da61d43fcaa39d68
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-10-27 21:44:13 -0600

    Remove unnecessary extra space below when zooming with the pipette

--- project/source/combined_scripts/pipette.gd
+++ project/source/combined_scripts/pipette.gd
@@ -6,6 +6,8 @@ class_name Pipe #TODO: Placeholder name since Pipette is already used in the old
 		has_tip = value
 		$SelectableCanvasGroup/PipetteWithTip.visible = has_tip
 		$TipCollision.disabled = not has_tip
+		$ExclusiveObjectHitbox/NoTipEnd.disabled = has_tip
+		$ExclusiveObjectHitbox/WithTipEnd.disabled = not has_tip
 
 
 func _on_exclusive_object_hitbox_entered_purview_of(area: ExclusiveArea2D) -> void:
@@ -14,7 +16,7 @@ func _on_exclusive_object_hitbox_entered_purview_of(area: ExclusiveArea2D) -> vo
 	var object_to_zoom := area.get_parent() as CollisionObject2D
 	if object_to_zoom:
 		# Zoom in on the object, with extra room above it for this pipette.
-		var rect := Util.get_global_bounding_box(object_to_zoom).grow_side(1, Util.get_global_bounding_box(self).size.y + 5).grow_side(3, 100)
+		var rect := Util.get_global_bounding_box(object_to_zoom).grow_side(1, Util.get_global_bounding_box(self).size.y + 5)
 		Game.main.focus_camera_on_rect(rect, 1.5)
 
 func _on_exclusive_object_hitbox_left_purview_of(area: ExclusiveArea2D) -> void:
