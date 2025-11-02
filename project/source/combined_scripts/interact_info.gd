dd3546c1981f2e5a1b9627e4826ae070d9bce31a
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-02 13:29:40 -0700

    Make `InteractInfo` into an exportable resource
    The intent is to allow certain interactables (like
    `SelectableComponent`s) to just directly export the `InteractInfo` they
    will use instead of having to export all of the parameters separately.
    
    The `_init` function also needed to be given default values for all of
    its parameters, since exported resources need to be constructible with
    just `_init()` (no arguments) to be used in scenes.

--- project/source/combined_scripts/interact_info.gd
+++ project/source/combined_scripts/interact_info.gd
@@ -1,6 +1,7 @@
 ## Represents an interaction that an object can perform. This is used for displaying button prompts
 ## and interaction directions.
 class_name InteractInfo
+extends Resource
 
 
 enum Kind
@@ -11,21 +12,22 @@ enum Kind
 	INSPECT, ## E by default. Used to zoom in on things.
 }
 
+
 ## variable holding the type of interaction. Primary by default.
-var kind: Kind = Kind.PRIMARY
+@export var kind: Kind = Kind.PRIMARY
 
 ## Displayed in the button prompt.
-var description: String = ""
+@export var description: String = ""
 
 ## If this is set to [code]false[/code], then this interaction can be [i]targeted[/i], but pressing
 ## the button will not do anything. Setting this to [code]false[/code] will [i]also[/i] show a
 ## greyed out button prompt. As an example, the pipette can't interact with the pipette tip box when
 ## it already has a tip, so it's better to show a greyed out button prompt that says something like
 ## "Left click: Add tip (already has tip).
-var allowed: bool = true
+@export var allowed: bool = true
 
 
-func _init(p_kind: Kind, p_description: String, p_allowed: bool = true) -> void:
+func _init(p_kind: Kind = Kind.PRIMARY, p_description: String = "", p_allowed: bool = true) -> void:
 	kind = p_kind
 	description = p_description
 	allowed = p_allowed
