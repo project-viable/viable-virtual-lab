ea116c8482681441559c4086cadf6053e7cb9552
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-04 00:00:53 -0700

    Make the gel mold move the bands based on voltage

--- project/source/combined_scripts/gel_mold.gd
+++ project/source/combined_scripts/gel_mold.gd
@@ -4,3 +4,14 @@ extends LabBody
 
 ## Voltage, where positive is down the gel.
 @export var voltage: float = 0.0
+
+
+func _physics_process(delta: float) -> void:
+	for i in 5:
+		for s in get_well(i + 1).substances:
+			if s is DNASolutionSubstance:
+				s.run_voltage(voltage, delta * LabTime.time_scale, 1.0)
+
+## Wells are numbered 1 to 5.
+func get_well(i: int) -> ContainerComponent:
+	return get_node_or_null("Subscene/Well%s" % [i])
