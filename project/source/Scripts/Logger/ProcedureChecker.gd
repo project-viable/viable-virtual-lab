353fba35ac375e80bb70ff09b14b684bdf6f897f
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-07 14:17:21 -0700

    Remove all unused old scripts and references to them, and move current scripts to a `scripts` directory

--- project/source/Scripts/Logger/ProcedureChecker.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Node2D
-
-@export var check_strategies: Array[MistakeChecker]
-
-func check_action(params: Dictionary) -> void:
-	for strategy in check_strategies:
-		strategy.check_action(params)
--- project/source/scripts/main.gd
+++ project/source/scripts/main.gd
@@ -8,15 +8,12 @@ const INTERACTION_PROMPT_SCENE := preload("res://scenes/interaction_prompt.tscn"
 ## This will be called with [code]null[/code] when returning to the workspace.
 signal camera_focus_owner_changed(focus_owner: Node)
 
-
-@export var check_strategies: Array[MistakeChecker]
 @export var all_modules: Array[ModuleData]
 
 # TODO (update): This is essentially public, so we should consider using a convention to make that
 # clear, like naming it in PascalCase.
 var current_module_scene: Node = null
 
-var module_directory: String = "res://Modules/"
 var module_button: PackedScene = load("res://scenes/module_select_button.tscn")
 
 
@@ -83,9 +80,9 @@ func _ready() -> void:
 	LabLog.connect("logs_cleared", Callable(self, "_on_Logs_Cleared"))
 
 	set_log_notification_counts()
-	$Menu/LogButton/LogMenu.set_tab_icon(1, load("res://Images/Dot-Blue.png"))
-	$Menu/LogButton/LogMenu.set_tab_icon(2, load("res://Images/Dot-Yellow.png"))
-	$Menu/LogButton/LogMenu.set_tab_icon(3, load("res://Images/Dot-Red.png"))
+	$Menu/LogButton/LogMenu.set_tab_icon(1, load("res://textures/old/Dot-Blue.png"))
+	$Menu/LogButton/LogMenu.set_tab_icon(2, load("res://textures/old/Dot-Yellow.png"))
+	$Menu/LogButton/LogMenu.set_tab_icon(3, load("res://textures/old/Dot-Red.png"))
 
 	var max_resolution := DisplayServer.screen_get_usable_rect().size
 
@@ -216,10 +213,6 @@ func _load_module(module: ModuleData) -> void:
 	Cursor.virtual_mouse_position = get_global_mouse_position()
 	set_paused(false)
 
-func check_action(params: Dictionary) -> void:
-	for strategy in check_strategies:
-		strategy.check_action(params)
-
 func unload_current_module() -> void:
 	LabLog.clear_logs()
 	DepthManager.clear_layers()
@@ -235,63 +228,6 @@ func set_scene(scene: PackedScene) -> void:
 	$%Scene.call_deferred("add_child", new_scene)
 	current_module_scene = new_scene
 	#$Camera.reset()
-
-func get_deepest_subscene_at(pos: Vector2) -> Node:
-	var result: Node = null
-
-	var cast_params := PhysicsPointQueryParameters2D.new()
-	cast_params.position = pos
-	cast_params.collision_mask = 0b10
-	cast_params.collide_with_bodies = false
-	cast_params.collide_with_areas = true
-	var cast_result: Array[Dictionary] = get_world_2d().direct_space_state.intersect_point(cast_params)
-
-	if len(cast_result) > 0:
-		#We found results
-		for object in cast_result:
-			if not object['collider'] is LabObject: #the allowed area of a subscene is not a LabObject
-				if object['collider'].get_parent() is SubsceneManager: #^but its parent is the SubsceneManager
-					if object['collider'].get_parent().subscene_active and (result == null or object['collider'].get_parent().count_subscene_depth() > result.count_subscene_depth()):
-						result = object['collider'].get_parent()
-	
-	return result
-
-func _unhandled_input(event: InputEvent) -> void:
-	###Check if they clicked a LabObject first
-	#check if there's any labobjects that need to deal with that input
-	#Using the normal object picking (collision objects' input signals) doesn't give us the control we need
-	if event.is_action_pressed("DragLabObject"):
-		var cast_params := PhysicsPointQueryParameters2D.new()
-		cast_params.position = get_global_mouse_position()
-		cast_params.collision_mask = 0b10
-		cast_params.collide_with_bodies = true
-		cast_params.collide_with_areas = true
-		var cast_result: Array[Dictionary] = get_world_2d().direct_space_state.intersect_point(cast_params)
-		
-		if len(cast_result) > 0:
-			#We found results: now we need to make sure only objects in the subscene we clicked in (if any) can get this input
-			var deepest_subscene := get_deepest_subscene_at(get_global_mouse_position())
-			
-			var pick_options: Array[LabObject] = []
-			for result in cast_result:
-				if result['collider'] is LabObject and (result['collider'].get_subscene_manager_parent() == deepest_subscene or (result['collider'] == deepest_subscene and not result['collider'].subscene_active)):
-					pick_options.append(result['collider'])
-			
-			var best_pick: LabObject = null
-			for object in pick_options:
-				#draggables are better than non draggables, and high z indexes are tie breakers
-				if best_pick == null or (
-					object.draggable and not best_pick.draggable) or (
-					object.z_index > best_pick.z_index and object.draggable == best_pick.draggable):
-						best_pick = object
-			
-			if best_pick:
-				if best_pick.draggable:
-					best_pick.start_dragging()
-				else:
-					best_pick.on_user_action()
-				get_viewport().set_input_as_handled()
-				return
 
 func set_popup_border_color(color: Color) -> void:
 	$Menu/LabLogPopup/Border.border_color = color
