81f00f398d41110e6246ec662424eaf2dd8ae416
EveDiby <94596452+EveDiby@users.noreply.github.com>
2025-11-12 22:35:52 -0700

    Add Imager door functionality
    Being able to take the gel mold out of the imager is now possible

--- scripts/imager_gel_band_logic.gd
+++ scripts/imager_gel_band_logic.gd
@@ -4,19 +4,23 @@ var band_texture: Texture2D = preload("res://textures/gel_bands/Gel_Well_Top_Vie
 var _is_light_on := false
 
 func _draw() -> void:
-	var gel := $AttachmentInteractableArea.contained_object as GelMold
-	if not gel or not _is_light_on: return
-	for i:int in gel.num_wells():
-		var well_sprite := _get_well_sprite(i + 1)
-		var well: ContainerComponent = gel.get_well(i + 1)
-		if not well or not well_sprite: continue
-		well_sprite.show()
-		for s in well.substances:
-			if s is DNASolutionSubstance:
-				# 130 / 0.45 is about the distance in local sprite coordinates to the end of the
-				# gel.
-				var pos: Vector2 = Vector2.DOWN * s.position * 130.0 / 0.45
-				band_texture.draw(well_sprite.get_canvas_item(), pos)
+	if $AttachmentInteractableArea.contained_object is GelMold:
+		var gel := $AttachmentInteractableArea.contained_object as GelMold
+		if not gel or not _is_light_on: return
+		for i:int in gel.num_wells():
+			var well_sprite := _get_well_sprite(i + 1)
+			var well: ContainerComponent = gel.get_well(i + 1)
+			if not well or not well_sprite: continue
+			well_sprite.show()
+			for s in well.substances:
+				if s is DNASolutionSubstance:
+					# 130 / 0.45 is about the distance in local sprite coordinates to the end of the
+					# gel.
+					var pos: Vector2 = Vector2.DOWN * s.position * 130.0 / 0.45
+					band_texture.draw(well_sprite.get_canvas_item(), pos)
+	else:
+		$AttachmentInteractableArea.remove_object()
+		$AttachmentInteractableArea.contained_object = null
 
 
 func _process(_delta: float) -> void:
@@ -28,71 +32,17 @@ func _process(_delta: float) -> void:
 
 func _get_well_sprite(i: int) -> Node2D:
 	return get_node_or_null("%%Well%s" % [i])
-
-#func get_well_x_pos(i: int) -> float:
-#	match i:
-#		0:
-#			return $Well1.position.x
-#		1:
-#			return $Well2.position.x
-#		2:
-#			return $Well3.position.x
-#		3:
-#			return $Well4.position.x
-#		4:
-#			return $Well5.position.x
-#		_:
-#			return 0
-#	
-#func get_band_y_pos(i: int, fragment_size: float) -> float:
-#	match i:
-#		0:
-#			return $Well1.position.y + (fragment_size*3.5)
-#		1:
-#			return $Well2.position.y + (fragment_size*3.5)
-#		2:
-#			return $Well3.position.y + (fragment_size*3.5)
-#		3:
-#			return $Well4.position.y + (fragment_size*3.5)
-#		4:
-#			return $Well5.position.y + (fragment_size*3.5)
-#		_:
-#			return 0
-#	
-#func on_gel_inserted() -> void:
-#	base_gel_sprite = $GelRigTopView
-#	base_wells_sprite = $GelWellTopViewLadderMultiple
-#	
-#	if get_UV_state() == true:
-#		print("starting imaging")
-#		if $AttachmentInteractableArea.object_placed != null:
-#			gel = $AttachmentInteractableArea.object_placed
-#			display_gel_bands()
-#		else:
-#			print("nothing is inside the imager.")
-#	else:
-#		display_blank_gel_bands()
-#		print("UV light is off. Gel will be blank")
+	
+func on_gel_removed() -> void:
+	if $AttachmentInteractableArea.contained_object != null:
+		$AttachmentInteractableArea.contained_object.global_position = Vector2(1650, 350)
+		$AttachmentInteractableArea.remove_object()
+		$AttachmentInteractableArea.contained_object = null
+		
 
 func _on_uv_light_pressed() -> void:
 	_is_light_on = not _is_light_on
 
-#func on_gel_removed() -> void:
-#	if $AttachmentInteractableArea.contained_object != null:
-#		base_gel_sprite.visible = false
-#		base_wells_sprite.visible = false
-#		band_texture = null
-#		$AttachmentInteractableArea.remove_object()
-#	else:
-#		for body: LabBody in  $AttachmentInteractableArea.get_overlapping_bodies():
-#			if body.name == "GelMold":
-#				$AttachmentInteractableArea.contained_object = body
-#				print("there is something already in the imager")
-#
-#func display_blank_gel_bands() -> void:
-#	base_gel_sprite.visible = true
-#	base_wells_sprite.visible = true
-#	
 #func analyze_gel(gel: LabBody) -> void:
 #	for i: int in gel.num_wells():
 #		var well: ContainerComponent = gel.get_well(i)
