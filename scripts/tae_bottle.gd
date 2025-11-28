95477c1b01cfb64934d0483ee1deac63a41c59c8
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-28 16:41:15 -0700

    Make the TAE bottle pourable and pipettable

---
+++ scripts/tae_bottle.gd
@@ -0,0 +1,15 @@
+extends LabBody
+
+
+var _num_pipettes: int = 0
+
+
+func _on_exclusive_area_2d_object_entered_purview(object: ExclusiveObjectHitbox) -> void:
+	_num_pipettes += 1
+	if _num_pipettes == 1:
+		$AnimationPlayer.play("fade_front")
+
+func _on_exclusive_area_2d_object_left_purview(object: ExclusiveObjectHitbox) -> void:
+	_num_pipettes -= 1
+	if _num_pipettes == 0:
+		$AnimationPlayer.play_backwards("fade_front")
