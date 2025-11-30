499a2a1721d52489d15f9de842d934984ec3373a
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-30 15:46:39 -0700

    Add some slosh physics to `SubstanceDisplayPolygon`

--- scripts/substance_display_polygon.gd
+++ scripts/substance_display_polygon.gd
@@ -3,28 +3,44 @@
 class_name SubstanceDisplayPolygon
 extends Polygon2D
 
+
+# Percentage of angular velocity removed per second.
+const SLOSH_ANGULAR_DAMP: float = 10
+const SLOSH_PENDULUM_LENGTH: float = 100
+const SLOSH_PENDULUM_MASS: float = 1
+const SLOSH_PENDULUM_MOMENT_OF_INERTIA = SLOSH_PENDULUM_MASS * pow(SLOSH_PENDULUM_MASS, 2)
+const SLOSH_GRAVITY: float = 1500
+
+
 @export var source: ContainerComponent
 
+
+# We model the sloshing of the fluid as a pendulum deciding the fluid's down direction.
+var _global_gravity: Vector2
+var _slosh_target_θ: float
+var _slosh_θ: float
+# Angular velocity.
+var _slosh_ω: float = 0
 
 var _fill_area_cache := PolygonFillAreaCache.new()
 # Transforms a local point into a space where [member _last_cached_down_dir] points in the negative
 # y direction.
 var _local_to_fill := Transform2D()
+var _last_cached_down_dir: Vector2
 @onready var _container_vertices: PackedVector2Array = polygon
 @onready var _container_tris: PackedInt32Array = Geometry2D.triangulate_polygon(_container_vertices)
-@onready var _last_cached_down_dir: Vector2 = Util.direction_to_local(self, Vector2.DOWN)
 
 
 static var _shader: Shader = preload("res://shaders/substance_polygon.gdshader")
 
 
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_TRANSFORM_CHANGED:
-		var down_dir := Util.direction_to_local(self, Vector2.DOWN)
-		if not down_dir.is_equal_approx(_last_cached_down_dir):
-			_set_down_dir(down_dir)
-
 func _enter_tree() -> void:
+	_global_gravity = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
+	_global_gravity *= SLOSH_GRAVITY
+	_slosh_target_θ = Util.direction_to_local(self, _global_gravity).angle()
+	_slosh_θ = _slosh_target_θ
+	_last_cached_down_dir = Vector2.from_angle(_slosh_θ)
+
 	material = ShaderMaterial.new()
 	material.shader = _shader
 	set_notify_transform(true)
@@ -32,8 +48,22 @@ func _enter_tree() -> void:
 func _ready() -> void:
 	_set_down_dir(_last_cached_down_dir)
 
+func _physics_process(delta: float) -> void:
+	_slosh_target_θ = Util.direction_to_local(self, _global_gravity).angle()
+	var g := Util.direction_to_local(self, _global_gravity).length() / 1000
+	var θ := angle_difference(_slosh_target_θ, _slosh_θ)
+	var τ := -g * SLOSH_PENDULUM_MASS * SLOSH_PENDULUM_LENGTH * sin(θ)
+	_slosh_ω += τ / SLOSH_PENDULUM_MOMENT_OF_INERTIA * delta
+	var prev_slosh_ω := _slosh_ω
+	_slosh_ω -= _slosh_ω * SLOSH_ANGULAR_DAMP * delta
+	# Damping should not be able to cause the sloshing to move in the other direction.
+	if sign(_slosh_ω) != sign(prev_slosh_ω): _slosh_ω = 0
+	_slosh_θ += _slosh_ω * delta
+
 func _process(_delta: float) -> void:
 	if not source: return
+
+	_set_down_dir_if_changed(Vector2.from_angle(_slosh_θ))
 
 	var total_container_volume: float = source.container_volume
 	var total_area := _fill_area_cache.get_total_area()
@@ -77,6 +107,15 @@ func get_substance_at(pos: Vector2) -> Substance:
 ## coordinates.
 func get_substance_at_global(pos: Vector2) -> Substance:
 	return get_substance_at(to_local(pos))
+
+## Get the down direction of the top of the fluid, in local coordinates. This will often not
+## correspond with the global down direction if the fluid is sloshing.
+func get_down_dir() -> Vector2:
+	return Vector2.ZERO
+
+func _set_down_dir_if_changed(down: Vector2) -> void:
+	if not down.is_equal_approx(_last_cached_down_dir):
+		_set_down_dir(down)
 
 func _set_down_dir(down: Vector2) -> void:
 	_last_cached_down_dir = down
