f35401d35b83ca4ad1c608f9ff98601dd480beb6
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-11-30 22:55:54 -0700

    Decrease spill rate for higher viscosity

--- scripts/spill_component.gd
+++ scripts/spill_component.gd
@@ -6,17 +6,22 @@ extends Node2D
 ## spilled into [member target_container].
 
 
+# Spill rate of fluids with zero viscosity in mL/s.
+const DEFAULT_SPILL_RATE: float = 20
+
+
+
 ## Substance polygon displaying the container to be spilled. If a substance is displayed at the
 ## location of this node, then it will be spilled.
 @export var substance_display_polygon: SubstanceDisplayPolygon
 ## The container to add the spilled substance to.
 @export var target_container: ContainerComponent
-## Rate of spillage in mL per second (in real time rather than lab time).
-@export_custom(PROPERTY_HINT_NONE, "suffix:mL/s") var spill_rate: float = 1
 
 
 func _physics_process(delta: float) -> void:
 	if not substance_display_polygon or not target_container: return
 
 	var s := substance_display_polygon.get_substance_at_global(global_position)
-	if s: target_container.add(s.take_volume(spill_rate * delta))
+	if s:
+		var pour_factor := 1.0 - ease(s.get_viscosity(), 0.4)
+		target_container.add(s.take_volume(DEFAULT_SPILL_RATE * pour_factor * delta))
