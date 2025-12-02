4ff4f0766fb5b017bfc1bb90e6ea4f366b29b65f
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-12-01 21:09:33 -0700

    Remove container volume logic from scoopula
    This is already handled by `ContainerComponent`, and was leading to
    weird behavior when trying to dispense into a container with more than
    one gram of substance in it.

--- scripts/scoopula_use_component.gd
+++ scripts/scoopula_use_component.gd
@@ -25,33 +25,11 @@ func start_use(area: InteractableArea, kind: InteractInfo.Kind) -> void:
 
 	match kind:
 		InteractInfo.Kind.PRIMARY:
-			## If the container being interacted with isn't empty but the amount the user wants to scoop is greater than the
-			## amount in said container or how much the scoopula can hold, the user will be notified that they can't do that.
-			## Otherwise, the user can scoop like normal
-			if not area.container_component.substances.is_empty():
-				if vol_to_take > area.container_component.get_total_volume() or vol_to_take > container.container_volume:
-					print("too many ml added to scoopula")
-				else:
-					### Add contents to scoopula and update containers's substance volume
-					container.add_array(area.container_component.take_volume(vol_to_take))
-					print("My scoopula after scooping has volume of: ", container.get_total_volume())
-					
-					get_parent().find_child("FillSprite").visible = true
+			container.add_array(area.container_component.take_volume(vol_to_take))
+			get_parent().find_child("FillSprite").visible = true
 			
-			else:
-				print("container is empty. There is nothing to scoop")	
 		InteractInfo.Kind.SECONDARY:
-			if container.substances.is_empty():
-				print("scoopula is empty")
-			else:
-				## As long as the scoopula isn't empty and the container isn't full, the user can dispense substances from it
-				## Add contents to receiving container if it isn't full
-				## Reset scoopula
-				if not area.container_component.substances.is_empty():
-					area.container_component.add_array(container.take_volume(container.container_volume-area.container_component.get_total_volume()))
-				else:
-					area.container_component.add_array(container.take_volume(container.container_volume))
-				container.substances.clear()
-				get_parent().find_child("FillSprite").visible = false
+			area.container_component.add_array(container.take_volume(vol_to_dispense))
+			get_parent().find_child("FillSprite").visible = false
 		
 			
