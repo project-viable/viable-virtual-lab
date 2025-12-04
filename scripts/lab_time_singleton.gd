9e480cfb0d80158a8e07e491d3307e3580f0d259
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-12-03 17:37:43 -0700

    Move time of day handling to `LabTime` so it can be used elsewhere

--- scripts/lab_time_singleton.gd
+++ scripts/lab_time_singleton.gd
@@ -4,5 +4,29 @@ extends Node
 ## physics or animations would be annoying.
 
 
+# Initial time after midnight, in seconds.
+const INITIAL_TIME_AFTER_MIDNIGHT: float = 60 * 60 * 9
+
+
 ## Multiplier on the rate that time passes in the simulation.
 var time_scale: float = 1.0
+
+## Current time, in seconds, after midnight when the simulation started. This does [i]not[/i]
+## reset to zero after a day has passed.
+var time_after_midnight: float = INITIAL_TIME_AFTER_MIDNIGHT
+
+
+func _process(delta: float) -> void:
+	time_after_midnight += delta * time_scale
+
+## Hour that would be displayed on a 24-hour digital clock.
+func get_clock_hour() -> int:
+	return floor(fmod(time_after_midnight / (60.0 * 60.0), 24.0))
+
+## Minute that would be displayed on a digital clock.
+func get_clock_minute() -> int:
+	return floor(fmod(time_after_midnight / 60.0, 60.0))
+
+## Seconds that would be displayed on a digital clock.
+func get_clock_second() -> int:
+	return floor(fmod(time_after_midnight, 60.0))
--- scripts/wall_clock.gd
+++ scripts/wall_clock.gd
@@ -1,21 +1,14 @@
 extends Node2D
 
 
-# Initial time after midnight, in seconds.
-const INITIAL_TIME: float = 60 * 60 * 9
-
-
-var _cur_time: float = INITIAL_TIME
 var _time_held: float = 0.0
 var _is_held: bool = false
 
 
 func _process(delta: float) -> void:
-	_cur_time += delta * LabTime.time_scale
-
-	$HourHand.rotation = 2.0 * PI * fmod(_cur_time / (60.0 * 60.0), 12.0) / 12.0
-	$MinuteHand.rotation = 2.0 * PI * fmod(_cur_time / 60.0, 60.0) / 60.0
-	$SecondHand.rotation = 2.0 * PI * floor(fmod(_cur_time, 60.0)) / 60.0
+	$HourHand.rotation = 2.0 * PI * LabTime.get_clock_hour() / 12.0
+	$MinuteHand.rotation = 2.0 * PI * LabTime.get_clock_minute() / 60.0
+	$SecondHand.rotation = 2.0 * PI * LabTime.get_clock_second() / 60.0
 
 	_time_held += delta
 	if _is_held:
@@ -24,7 +17,6 @@ func _process(delta: float) -> void:
 func _on_selectable_component_started_holding() -> void:
 	_time_held = 0.0
 	_is_held = true
-
 
 func _on_selectable_component_stopped_holding() -> void:
 	_is_held = false
