821727e13c8a593f232c1bc40c496178ce42ad43
EveDiby <94596452+EveDiby@users.noreply.github.com>
2025-12-22 23:46:30 -0700

    Fixing array logic in report
    I reformatted the arrays for the well capacities and dna fragment sizes to appear as a list rather than an array object in square brackets

--- scripts/log_report.gd
+++ scripts/log_report.gd
@@ -7,7 +7,7 @@ var report_data := {
 	"scooped_agarose_powder": "0.0 mL of agarose powder add to ", 
 	"poured_tae_in_gel": "0.0 mL of TAE Buffer is in the gel", 
 	"microwave_time": "___ heated for ___ minutes", 
-	"gel_heated_temperature": "",
+	"gel_heated_temperature": "___ heated to ___ degrees Celsius",
 	"gel_concentration": "The gel's concentration is 0%", 
 	"gel_mixed": "Gel is not thoroughly mixed ",
 	"poured_tae_in_rig": "0.0 mL of TAE Buffer poured into GelRig", 
@@ -55,14 +55,14 @@ func save_game_data() -> void:
 func load_report_data() -> void:
 	rich_text_label.append_text("[center][b][font_size=36]Final Report[/font_size][/b][/center]\n")
 	
-	rich_text_label.append_text("\n [center][b][font_size=24]Preparing The Gel[/font_size][/b][/center]\n")
+	rich_text_label.append_text("\n[center][b][font_size=24]Preparing The Gel[/font_size][/b][/center]\n")
 	rich_text_label.append_text("\n" + report_data["scooped_agarose_powder"])
 	rich_text_label.append_text("\n" + report_data["poured_tae_in_gel"])
 	rich_text_label.append_text("\n" + report_data["microwave_time"])
 	rich_text_label.append_text("\n" + report_data["gel_heated_temperature"])
 	rich_text_label.append_text("\n" + report_data["gel_mixed"])
 	
-	rich_text_label.append_text("\n \n[center][b][font_size=24]Gel Electrophoresis Process[/font_size][/b][/center]\n")
+	rich_text_label.append_text("\n\n[center][b][font_size=24]Gel Electrophoresis Process[/font_size][/b][/center]\n")
 	rich_text_label.append_text("\n" + report_data["poured_tae_in_rig"])
 	rich_text_label.append_text("\n" + report_data["correct_comb_placement"])
 	rich_text_label.append_text("\n" + report_data["electrode_correct_placement"])
@@ -70,13 +70,15 @@ func load_report_data() -> void:
 	rich_text_label.append_text("\n" + report_data["voltage_run_time"])
 	rich_text_label.append_text("\n" + report_data["time_until_gel_analysis"])
 	
-	rich_text_label.append_text("\n \n[center][b][font_size=24]Gel Analysis[/font_size][/b][/center]\n")
+	rich_text_label.append_text("\n\n[center][b][font_size=24]Gel Analysis[/font_size][/b][/center]\n")
 	rich_text_label.append_text("\n" + report_data["gel_concentration"])
 	rich_text_label.append_text("\n" + report_data["well_max_capacity"])
-	rich_text_label.append_text("\n The used wells were hold " + report_data["gel_well_capacities"] + " mLs of dna samples, respectively")
-	rich_text_label.append_text("\n The dna samples had fragment sizes of " + report_data["dna_fragment_sizes"])
+	var well_cap_string: String = ("\nThe used wells hold " + ", ".join(report_data["gel_well_capacities"]) + " mLs of dna samples, respectively")
+	rich_text_label.append_text(well_cap_string)
+	var dna_frag_string: String = ("\nThe dna samples had fragment sizes of " +  ", ".join(report_data["dna_fragment_sizes"]))
+	rich_text_label.append_text(dna_frag_string)
 	
-	rich_text_label.append_text("\n \n [center][b][font_size=24]Final results[/font_size][/b][/center]\n")
+	rich_text_label.append_text("\n\n[center][b][font_size=24]Final results[/font_size][/b][/center]\n")
 	rich_text_label.append_text("\n" + report_data["final_results"])
 	
 func _on_button_pressed() -> void:
