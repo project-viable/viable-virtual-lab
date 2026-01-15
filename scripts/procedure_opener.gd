42ab3d5742f89102b86df5840f7a0ef4c04defc8
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-01-14 23:41:59 -0700

    Add a default help page to the journal

--- scripts/procedure_opener.gd
+++ scripts/procedure_opener.gd
@@ -5,7 +5,11 @@ extends Node
 
 ## Path to the journal page relative to [code]res://journal_pages/[/code].
 @export var journal_page_path: String = ""
+## Optional initial basic help page.
+@export var initial_help_page_path: String = ""
 
 
 func _ready() -> void:
 	Game.journal.set_procedure_page(journal_page_path)
+	if not initial_help_page_path.is_empty():
+		Game.journal.go_to_page(initial_help_page_path)
