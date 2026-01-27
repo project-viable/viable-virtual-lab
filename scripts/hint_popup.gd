e6ddb0ef053f3703631d52aae63e016fbeac8050
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-01-27 15:17:15 -0700

    Animate fade-in and fade-out of hint popups, and improve handling for hint conditions

--- scripts/hint_popup.gd
+++ scripts/hint_popup.gd
@@ -2,6 +2,15 @@ class_name HintPopup
 extends PreprocessedRichTextLabel
 
 
+enum HintState
+{
+	NOT_REQUESTED, ## The hint has not been requested yet.
+	QUEUED, ## The hint has been queued to be shown.
+	SHOWING, ## The hint is actively being displayed on the screen.
+	CONDITION_MET, ## The condition to hide this hint has been met, so it will never again be displayed.
+}
+
+
 # General hints. `is_condition_met` is set for these in `main.gd` and `main_interactable_system.gd`.
 var left_right_hint := Hint.new("Press #{prompt:interact_left}# and #{prompt:interact_right}# to move left and right in the lab")
 var journal_hint := Hint.new("Press #{prompt:toggle_journal}# to open the procedure")
@@ -13,40 +22,52 @@ var _cur_hint: Hint = null
 
 
 func _process(_delta: float) -> void:
-	if _cur_hint and not _cur_hint.is_condition_met: return
+	# Don't change the hint state until the animation has been fully played.
+	if _cur_hint and _cur_hint._state != HintState.CONDITION_MET or $AnimationPlayer.is_playing():
+		return
 
+	var _prev_hint := _cur_hint
 	_cur_hint = null
-
-	# Clear hints whose conditions have been met already.
-	var next_hint_index := _hint_queue.find_custom(func(h: Hint) -> bool: return not h.is_condition_met)
-	if next_hint_index >= 0:
-		_hint_queue.assign(_hint_queue.slice(next_hint_index))
 
 	if _hint_queue: _cur_hint = _hint_queue.pop_front()
 
+	# Fade out hint before fading in new one.
+	if _prev_hint:
+		$AnimationPlayer.play_backwards("fade_in")
+		await $AnimationPlayer.animation_finished
 
 	if _cur_hint:
 		custom_text = _cur_hint.text
-		show()
-	else:
-		hide()
+		_cur_hint._state = HintState.SHOWING
+		$AnimationPlayer.play("fade_in")
 
 ## Request that the hint [param hint] be shown. If [code]hint.is_condition_met[/code] is
 ## [code]true[/code], then the hint will not be displayed. If a hint is currently being displayed,
 ## then [param hint] will be placed in the back of the hint queue, to be displayed after all other
 ## enqueued hints have been displayed.
 func request_hint(hint: Hint) -> void:
-	if not hint.is_condition_met: _hint_queue.push_back(hint)
-
+	if hint._state == HintState.NOT_REQUESTED:
+		hint._state = HintState.QUEUED
+		_hint_queue.push_back(hint)
 
 ## A hint to display.
 class Hint:
 	## Text to display in the label. This will be used as the
 	## [member PreprocessedRichTextLabel.custom_text] of a [PreprocessedRichTextLabel].
 	var text: String = ""
-	## If set to [code]true[/code], this will no longer be displayed.
-	var is_condition_met: bool = false
+
+	var _state := HintState.NOT_REQUESTED
 
 
 	func _init(p_text: String) -> void:
 		text = p_text
+
+	## Request that this hint be shown.
+	func request() -> void:
+		Game.hint_popup.request_hint(self)
+
+	## Should be called when the condition for this hint has been met. If the hint is currently
+	## displayed on-screen, then this will hide the hint. Otherwise, this will do nothing.
+	func notify_condition_met() -> void:
+		if _state == HintState.SHOWING:
+			_state = HintState.CONDITION_MET
